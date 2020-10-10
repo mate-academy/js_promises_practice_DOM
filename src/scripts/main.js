5a721d4a95c7f0601e3fa5a2956169b87c96c881
Ihor Hranetskyi <graniecki20@gmail.com>
2020-10-10 21:55:21 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,56 @@
+/* eslint-disable no-console */
 'use strict';
+
+const body = document.body;
+
+function firstPromise() {
+  const resolver = (resolve, reject) => {
+    body.addEventListener('click', () => {
+      resolve('Left click');
+    });
+    setTimeout(() => reject(new Error('more than 5 seconds passed')), 5000);
+  };
+
+  return new Promise(resolver);
+}
+
+const promise1 = firstPromise();
+
+promise1
+  .then(result => console.log(result))
+  .catch(error => console.error(error));
+
+function secondPromise() {
+  const resolver = (resolve, reject) => {
+    body.addEventListener('click', () => {
+      resolve('Left click');
+    });
+
+    body.addEventListener('contextmenu', () => {
+      resolve('Right click');
+    });
+  };
+
+  return new Promise(resolver);
+}
+
+const promise2 = secondPromise();
+
+promise2
+  .then(result => console.log(result));
+
+function createEventPromise(handler) {
+  const resolver = (resolve, reject) => {
+    body.addEventListener(handler, () => {
+      resolve(handler);
+    });
+  };
+
+  return new Promise(resolver);
+}
+
+const promise3 = createEventPromise('click');
+const promise4 = createEventPromise('contextmenu');
+
+Promise.all([promise3, promise4])
+  .then(() => console.log('Left and Right clicks'));
