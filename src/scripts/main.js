b60b97344299f4083739ea8045e75561d030af86
DmitryArhipenko <42931799+DmitryArhipenko@users.noreply.github.com>
2020-10-22 15:19:11 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,73 @@
+/* eslint-disable prefer-promise-reject-errors */
+/* eslint-disable no-console */
+
 'use strict';
+
+function firstPromise() {
+  return new Promise((resolve, reject) => {
+    document.addEventListener('click', () => {
+      resolve('Resolve 1');
+    });
+
+    setTimeout(() => {
+      reject('Reject 1');
+    }, 5000);
+  });
+}
+
+function secondPromise() {
+  return new Promise((resolve, reject) => {
+    document.addEventListener('click', (event) => {
+      event.preventDefault();
+      resolve('Resolve 2');
+    });
+
+    document.addEventListener('contextmenu', (event) => {
+      event.preventDefault();
+      resolve('Resolve 2');
+    });
+  });
+}
+
+function thirdPromise() {
+  return new Promise((resolve, reject) => {
+    let click = false;
+    let contextmenu = false;
+
+    document.addEventListener('mousedown', (event) => {
+      event.preventDefault();
+
+      if (event.button === 0) {
+        click = true;
+      }
+
+      if (event.button === 2) {
+        contextmenu = true;
+      }
+
+      if (click && contextmenu) {
+        resolve('Resolve 3');
+      }
+    });
+  });
+}
+
+const promiseOne = firstPromise();
+const promiseTwo = secondPromise();
+const promiseThree = thirdPromise();
+
+promiseOne
+  .then(
+    result => console.log(result),
+    error => console.error(error)
+  );
+
+promiseTwo
+  .then(
+    result => console.log(result)
+  );
+
+promiseThree
+  .then(
+    result => console.log(result)
+  );
