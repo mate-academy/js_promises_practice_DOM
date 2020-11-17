8f116fc126beb47b920358b86b2cb5a62fe28022
dmitry <akulinind@gmail.com>
2020-11-17 19:16:00 +0300

    add 3 promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.onclick = () => resolve('First resolved!');
+  setTimeout(() => reject(new Error('First rejected!')), 5000);
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () =>
+    resolve('Second resolved!'));
+
+  document.addEventListener('contextmenu', () =>
+    resolve('Second resolved!'));
+});
+const leftButtonPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => resolve());
+});
+
+const rightButtonPromise = new Promise((resolve, reject) => {
+  document.addEventListener('contextmenu', () => resolve());
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  leftButtonPromise
+    .then(() => rightButtonPromise)
+    .then(() => {
+      resolve('Third promise resolved');
+    });
+});
+/* eslint-disable no-console */
+
+firstPromise
+  .then(result => console.log(result))
+  .catch((error) => console.log(error));
+
+secondPromise
+  .then(result => console.log(result));
+thirdPromise.then(result => console.log(result));
