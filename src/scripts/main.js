e591fae3525342178ac800f1d8f188952b182c28
Ivan Panchenko <ivanpanchenkorulit@gmail.com>
2021-03-30 16:14:06 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,63 @@
 'use strict';
+
+function addMessage(className, text) {
+  const message = document.createElement('div');
+
+  message.innerText = text;
+  message.dataset.qa = 'notification';
+  message.classList.add(className);
+  document.body.append(message);
+}
+
+const promiseOne = new Promise((resolve, reject) => {
+  document.addEventListener('click', resolve);
+
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+});
+
+promiseOne
+  .then(() => {
+    addMessage('success', 'First promise was resolved');
+  })
+  .catch(() => {
+    addMessage('warning', 'First promise was rejected');
+  });
+
+const promiseTwo = new Promise(resolve => {
+  document.addEventListener('mousedown', (mouseEvent) => {
+    if (mouseEvent.button === 0 || mouseEvent.button === 2) {
+      resolve();
+    }
+  });
+});
+
+promiseTwo
+  .then(() => {
+    addMessage('success', 'Second promise was resolved');
+  });
+
+const proviseThree = new Promise(resolve => {
+  let hasPressLeftButton = false;
+  let hasPressRightButton = false;
+
+  document.addEventListener('mousedown', (mouseEvent) => {
+    if (mouseEvent.button === 0) {
+      hasPressLeftButton = true;
+    }
+
+    if (mouseEvent.button === 2) {
+      hasPressRightButton = true;
+    }
+
+    if (hasPressLeftButton && hasPressRightButton) {
+      resolve();
+    }
+  });
+});
+
+proviseThree
+  .then(() => {
+    addMessage('success', 'Third promise was resolved');
+  });
