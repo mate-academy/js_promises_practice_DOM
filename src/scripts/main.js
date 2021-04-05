d89353f0b4ee6c9bec6bb143206dc7eeaa652838
Olga Briukhanova <oaleksandrovva@gmail.com>
2021-04-05 16:32:36 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,68 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+function createMessage(classOfMessage, textOfMessage) {
+  const message = document.createElement('div');
+
+  message.className = classOfMessage;
+  message.dataset.qa = 'notification';
+  message.textContent = textOfMessage;
+
+  body.append(message);
+}
+
+const firstPromise = new Promise((resolve, reject) => {
+  body.addEventListener('mousedown', () => {
+    resolve();
+  });
+
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+});
+
+firstPromise
+  .then(() => {
+    createMessage('success', 'First promise was resolved');
+  })
+  .catch(() => {
+    createMessage('warning', 'First promise was rejected');
+  });
+
+const secondPromise = new Promise((resolve, reject) => {
+  body.addEventListener('mousedown', (mouseEvent) => {
+    if (mouseEvent.button === 0 || mouseEvent.button === 2) {
+      return resolve();
+    }
+  });
+});
+
+secondPromise
+  .then(() => {
+    createMessage('success', 'Second promise was resolved');
+  });
+
+const thirdPromise = new Promise((resolve, reject) => {
+  let isLeftMouseClick = false;
+  let isRightMouseClick = false;
+
+  body.addEventListener('mousedown', (mouseEvent) => {
+    if (mouseEvent.button === 0) {
+      isRightMouseClick = true;
+    }
+
+    if (mouseEvent.button === 2) {
+      isLeftMouseClick = true;
+    }
+
+    if (isLeftMouseClick && isRightMouseClick) {
+      return resolve();
+    }
+  });
+});
+
+thirdPromise
+  .then(() => {
+    createMessage('success', 'Third promise was resolved');
+  });
