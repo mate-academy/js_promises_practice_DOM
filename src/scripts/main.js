9a60328c7cbcbf373310ac8f9ccd6bd0257bf6a5
Nikita Puhovskii <cheliojul@gmail.com>
2021-04-06 10:16:11 +0300

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,70 @@
 'use strict';
+
+const doc = document;
+
+function createMessage(className, text) {
+  const message = document.createElement('span');
+
+  message.className = className;
+  message.setAttribute('data-qa', 'notification');
+  message.innerText = text;
+
+  doc.append(message);
+};
+
+const firstPromise = new Promise((resolve, reject) => {
+  doc.addEventListener('click', () => {
+    resolve(`First promise was resolved`);
+  });
+
+  setTimeout(() => {
+    reject(new Error(`First promise was rejected`));
+  }, 3000);
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  doc.addEventListener('click', (clickEvent) => {
+    const { button } = clickEvent;
+
+    if (button === 0 || button === 2) {
+      resolve('Second promise was resolved');
+    }
+  });
+});
+
+const thirdPromise = new Promise((resolve) => {
+  let isLeftButtonClicked = false;
+  let isRightButtonClicked = false;
+
+  document.addEventListener('mousedown', (clickEvent) => {
+    const { button } = clickEvent;
+
+    if (button === 0) {
+      isLeftButtonClicked = !isLeftButtonClicked;
+    }
+
+    if (button === 2) {
+      isRightButtonClicked = !isRightButtonClicked;
+    }
+
+    if (isRightButtonClicked && isLeftButtonClicked) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+firstPromise
+  .then((text) => {
+    createMessage(text, 'success');
+  })
+  .catch(() => {
+    createMessage('First promise was rejected', 'warning');
+  });
+
+secondPromise.then((text) => {
+  createMessage(text, 'success');
+});
+
+thirdPromise.then((text) => {
+  createMessage('First promise was resolved', 'success');
+});
