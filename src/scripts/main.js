ab5a116ecfb7fdd14dcf7d681cc03186ada55351
Ihor Drul <druleger@gmail.com>
2021-04-09 11:31:35 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,57 @@
 'use strict';
+
+const createNotification = (message, classN) => {
+  const element = document.createElement('div');
+
+  element.textContent = message;
+  element.className = classN;
+  element.dataset.qa = 'notification';
+  document.body.append(element);
+
+  setTimeout(() => {
+    element.remove();
+  }, 3000);
+};
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+firstPromise
+  .then(result => createNotification(result, 'success'))
+  .catch(error => createNotification(error, 'warning'));
+
+const secondPromise = new Promise(resolve => {
+  document.addEventListener('mousedown', () => {
+    resolve('Second promise was resolved');
+  });
+});
+
+secondPromise.then(result => createNotification(result, 'success'));
+
+const thirdPromise = new Promise(resolve => {
+  let leftClick = false;
+  let rightClick = false;
+
+  document.addEventListener('mousedown', e => {
+    if (e.button === 0) {
+      leftClick = true;
+    }
+
+    if (e.button === 2) {
+      rightClick = true;
+    }
+
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+thirdPromise.then(result => createNotification(result, 'success'));
