4e1eb3cc24092d50ca0a7187fe6e5f654a73731b
Maksym Matuliak <maksymmatuliak@gmail.com>
2021-04-11 11:06:47 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,65 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+function createFirstPromise() {
+  const resolver = function(resolve, reject) {
+    document.onclick = () => resolve;
+    setTimeout(() => reject(), 3000);
+  };
+
+  return new Promise(resolver);
+}
+
+function createSecondPromise() {
+  const resolver = (resolve, reject) => {
+    document.addEventListener('click', resolve);
+    document.addEventListener('contextmenu', resolve);
+  };
+
+  return new Promise(resolver);
+}
+
+function createThirdPromise() {
+  const resolver = (resolve, reject) => {
+    let isRightClicked = false;
+    let isLeftClicked = false;
+
+    document.addEventListener('click', () => {
+      if (isRightClicked) {
+        resolve();
+      }
+
+      isLeftClicked = true;
+    });
+
+    document.addEventListener('contextmenu', () => {
+      if (isLeftClicked) {
+        resolve();
+      }
+
+      isRightClicked = true;
+    });
+  };
+
+  return new Promise(resolver);
+}
+
+createFirstPromise().then(() => document.insertAdjacentHTML('afterBegin',
+  `<div class="success" data-qa="notification">
+    First promise was resolved
+  </div>`))
+  .catch(() => body.insertAdjacentHTML('afterBegin',
+    `<div class="warning" data-qa="notification">
+      First promise was rejected
+    </div>`));
+
+createSecondPromise().then(() => body.insertAdjacentHTML('afterBegin',
+  `<div class="success" data-qa="notification">
+    Second promise was resolved
+  </div>`));
+
+createThirdPromise().then(() => body.insertAdjacentHTML('afterBegin',
+  `<div class="success" data-qa="notification">
+    Third promise was resolved
+  </div>`));
