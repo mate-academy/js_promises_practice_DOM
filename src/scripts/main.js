43d9b9bd54a81fc3040c3264c91a1d301d1a07f8
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-04-14 13:28:58 +0300

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,57 @@
 'use strict';
+
+const addMessage = (message, className) => {
+  const logo = document.querySelector('.logo');
+
+  logo.insertAdjacentHTML('afterend',
+    `
+  <div class="${className}">
+  ${message}
+  </div>
+  `
+  );
+};
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+firstPromise
+  .then(value => addMessage(value, 'success'))
+  .catch(error => addMessage(error, 'warning'));
+
+new Promise(resolve => {
+  document.addEventListener('mousedown', (click) => {
+    click.preventDefault();
+
+    if (click.button === 0 || click.button === 2) {
+      click.preventDefault();
+      resolve('Second promise was resolved');
+    }
+  });
+}).then(value => addMessage(value, 'success'));
+
+new Promise(resolve => {
+  let leftButton = false;
+  let rightButton = false;
+
+  document.addEventListener('mousedown', click => {
+    if (click.button === 0) {
+      leftButton = true;
+    }
+
+    if (click.button === 2) {
+      rightButton = true;
+    }
+
+    if (leftButton && rightButton) {
+      resolve('Third promise was resolved');
+    }
+  });
+}).then(value => addMessage(value, 'success'));
