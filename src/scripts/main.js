4e9d5292c3999c988c17345eb2ddd62f78cb04c7
Svyat Krit <krit.mgmn@gmail.com>
2021-04-15 14:21:41 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,53 @@
 'use strict';
+
+const createMessage = (type, content) => {
+  const message = document.createElement('div');
+
+  switch (type) {
+    case 'success':
+      message.classList.add('success');
+      message.innerText = `${content} promise was resolved`;
+      break;
+    case 'warning':
+      message.classList.add('warning');
+      message.innerText = `${content} promise was rejected`;
+      break;
+  }
+
+  message.setAttribute('data-qa', 'notification');
+
+  document.body.append(message);
+};
+
+new Promise((resolve, reject) => {
+  document.addEventListener('click', () => resolve());
+
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+})
+  .then(
+    () => createMessage('success', 'First')
+  )
+  .catch(
+    () => createMessage('warning', 'First')
+  );
+
+new Promise((resolve) => {
+  document.addEventListener('click', () => resolve());
+  document.addEventListener('contextmenu', () => resolve());
+})
+  .then(
+    () => createMessage('success', 'Second')
+  );
+
+new Promise((resolve) => {
+  document.addEventListener('click', () => resolve('contextmenu'));
+  document.addEventListener('contextmenu', () => resolve('click'));
+})
+  .then((result) =>
+    new Promise((resolve) => document.addEventListener(result, () => resolve()))
+  )
+  .then(
+    () => createMessage('success', 'Third')
+  );
