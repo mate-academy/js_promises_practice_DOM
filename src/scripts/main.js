fe59b8fa492c419770f9b0de7bd12507fc609260
Max Zadorozhniy <max.zadorozhniy2001@gmail.com>
2021-04-15 17:34:16 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,53 @@
 'use strict';
+
+const createMessage = (text, className) => {
+  const h1 = document.querySelector('h1');
+
+  h1.insertAdjacentHTML('afterend',
+    `<div data-qa="notification" class="${className}">${text}</div>`
+  );
+};
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+firstPromise
+  .then(value => createMessage(value, 'success'))
+  .catch(error => createMessage(error, 'warning'));
+
+new Promise(resolve => {
+  document.addEventListener('mousedown', (click) => {
+    click.preventDefault();
+
+    if (click.button === 0 || click.button === 2) {
+      click.preventDefault();
+      resolve('Second promise was resolved');
+    }
+  });
+}).then(value => createMessage(value, 'success'));
+
+new Promise(resolve => {
+  let leftButton = false;
+  let rightButton = false;
+
+  document.addEventListener('mousedown', click => {
+    if (click.button === 0) {
+      leftButton = true;
+    }
+
+    if (click.button === 2) {
+      rightButton = true;
+    }
+
+    if (leftButton && rightButton) {
+      resolve('Third promise was resolved');
+    }
+  });
+}).then(value => createMessage(value, 'success'));
