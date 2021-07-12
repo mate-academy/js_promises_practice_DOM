d2f97e1be7f7569e406d5ea1d7b10b220d631946
korzh <korzh.alexey@yahoo.com>
2021-07-12 14:50:35 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,45 @@
 'use strict';
+
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+
+logo.style.cursor = 'pointer';
+
+new Promise((resolve, reject) => {
+  const block = document.createElement('div');
+
+  logo.addEventListener('click', () => {
+    block.innerText = 'First promise was resolved';
+    resolve(block);
+  });
+
+  setTimeout(() => {
+    block.innerText = 'Error: First promise was rejected';
+    reject(block);
+  }, 3000);
+})
+  .then(el => body.append(el))
+  .catch(el => body.append(el));
+
+new Promise(resolve => {
+  logo.addEventListener('click', () => {
+    const block = document.createElement('div');
+
+    block.innerText = 'Second promise was resolved';
+    resolve(block);
+  });
+})
+  .then(el => body.append(el));
+
+new Promise(resolve => {
+  const block = document.createElement('div');
+
+  logo.addEventListener('click', () => {
+    logo.addEventListener('contextmenu', (ev) => {
+      ev.preventDefault();
+      block.innerText = 'Third promise was resolved';
+      resolve(block);
+    });
+  });
+})
+  .then(el => body.append(el));
