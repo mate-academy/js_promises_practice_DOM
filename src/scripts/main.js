8599533911e575510b8604af995d89c6f54a02b8
Yaslik1033 <55955258+Yaslik1033@users.noreply.github.com>
2021-07-19 13:09:14 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,53 @@
 'use strict';
+
+let leftClick = false;
+let rightClick = false;
+
+document.addEventListener('click', () => {
+  leftClick = true;
+});
+
+document.addEventListener('contextmenu', () => {
+  rightClick = true;
+});
+
+new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+  setTimeout(() => reject(new Error('First promise was rejected')), 3000);
+}).then(text => addNotification(text, 'success'))
+  .catch(text => addNotification(text, 'warning'));
+
+new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  });
+
+  document.addEventListener('contextmenu', () => {
+    resolve('Second promise was resolved');
+  });
+}).then(text => addNotification(text, 'success'));
+
+new Promise(resolve => {
+  document.addEventListener('click', () => {
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+
+  document.addEventListener('contextmenu', () => {
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+}).then(text => addNotification(text, 'success'));
+
+function addNotification(text, notificationClass) {
+  document.body.insertAdjacentHTML('beforeend', `
+    <div 
+      class='notification ${notificationClass}'
+      data-qa="notification"
+    >${text}</div>
+  `);
+}
