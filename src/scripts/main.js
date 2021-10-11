78684bed7c47d8f5bd14210b71085265d2ef54b6
MaxStets <weawer458@gmail.com>
2021-10-12 00:13:48 +0300

    Solution -- promises_practic_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,57 @@
 'use strict';
+
+const body = document.body;
+const message = {
+  first: {
+    resolved: 'First promise was resolved',
+    rejected: 'First promise was rejected',
+  },
+  second: 'Second promise was resolved',
+  third: 'Third promise was resolved',
+};
+
+const printNotification = (text, classStatus) => {
+  const maskMessage = `
+    <div 
+      style="padding: 20px;" 
+      class="${classStatus}" 
+      data-qa="notification"
+      >
+        ${text}
+    </div>
+  `;
+
+  body.insertAdjacentHTML('beforeend', maskMessage);
+};
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve([message.first.resolved, 'success']);
+  });
+
+  setTimeout(() => reject(message.first.rejected), 3000);
+});
+
+firstPromise
+  .then(
+    resolve => printNotification(...resolve),
+    reject => printNotification(reject, 'warning')
+  );
+
+const secondPromise = new Promise(resolve => {
+  document.addEventListener('mousedown', (e) => {
+    if (e.target.tagName !== 'H1') {
+      resolve(message.second);
+    }
+  });
+});
+
+secondPromise.then(resolve => printNotification(resolve, 'success'));
+
+const thirdPromise = new Promise(resolve => {
+  document.addEventListener('contextmenu', () => {
+    resolve(message.second);
+  });
+});
+
+thirdPromise.then(resolve => printNotification(resolve, 'success'));
