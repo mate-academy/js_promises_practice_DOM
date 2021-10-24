94b22a22d39650ed151303dbbecd61cf6385b599
Ilona Shulha <ilon.sh.ilo@gmail.com>
2021-10-24 19:53:18 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,57 @@
 'use strict';
+
+const body = document.querySelector('body');
+const fall = ['error', 'First promise was rejected'];
+let countLeft = 0;
+let countRight = 0;
+
+function getMessage(result) {
+  body.insertAdjacentHTML('afterbegin', `
+  <div data-qa="notification" class="${result[0]}">
+  ${result[1]}
+  </div>
+  `);
+}
+
+body.addEventListener('contextmenu', (e) => {
+  countRight = 1;
+});
+
+const firstPromise = new Promise((resolve, reject) => {
+  body.addEventListener('click', () => {
+    countLeft = 1;
+    resolve(['success', 'First promise was resolved']);
+  });
+
+  setTimeout(() => {
+    if (countLeft === 0) {
+      reject(fall);
+    }
+  }, 3000);
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  body.addEventListener('mouseup', () => {
+    resolve(['success', 'Second promise was resolved']);
+  });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  body.addEventListener('mouseup', (e) => {
+    const key = e.button;
+
+    if ((key === 0 & countRight === 1) || (key === 2 & countLeft === 1)) {
+      resolve(['success', 'Third promise was resolved']);
+    };
+  });
+});
+
+firstPromise
+  .then(getMessage)
+  .catch(getMessage);
+
+secondPromise
+  .then(getMessage);
+
+thirdPromise
+  .then(getMessage);
