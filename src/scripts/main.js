aee9663a73aa56d4368118e714789c93cdd524c2
Mariia Kovach <mariiakovach123@gmail.com>
2021-11-22 12:26:07 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,57 @@
 'use strict';
+
+function createNotification(result) {
+  document.body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification" class="${result[0]}">
+      ${result[1]}
+    </div>
+  `);
+}
+
+const firstPromise = new Promise((resolve, reject) => {
+  const failure = ['error', 'First promise was rejected'];
+
+  document.body.addEventListener('click', () => {
+    resolve(['success', 'First promise was resolved']);
+  });
+
+  setTimeout(() => {
+    reject(failure);
+  }, 3000);
+});
+
+const secondPromise = new Promise(resolve => {
+  document.body.addEventListener('mousedown', () => {
+    resolve(['success', 'Second promise was resolved']);
+  });
+});
+
+const thirdPromise = new Promise(resolve => {
+  let leftClick = false;
+  let rightClick = false;
+
+  document.addEventListener('mousedown', e => {
+    switch (e.button) {
+      case 0:
+        leftClick = true;
+        break;
+      case 2:
+        rightClick = true;
+        break;
+    }
+
+    if (leftClick && rightClick) {
+      resolve(['success', 'Third promise was resolved']);
+    }
+  });
+});
+
+firstPromise
+  .then(createNotification)
+  .catch(createNotification);
+
+secondPromise
+  .then(createNotification);
+
+thirdPromise
+  .then(createNotification);
