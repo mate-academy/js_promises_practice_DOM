cbfdc6d936f99e2dbab89393bd480f5539f764ca
Vasiliy <voronvasia@gmail.com>
2022-01-01 23:51:28 +0200

    task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,81 @@
 'use strict';
+
+const root = document.querySelector('body');
+const firstSuccess = `
+  <div
+    data-qa="notification"
+    class="success"
+  >First promise was resolved</div>`;
+const firstWarning = `
+  <div
+    data-qa="notification"
+    class="warning"
+  >First promise was rejected</div>`;
+
+const firstPomise = new Promise((resolve, reject) => {
+  document.addEventListener('mousedown', () => {
+    resolve(firstSuccess);
+  });
+
+  setTimeout(() => {
+    reject(firstWarning);
+  }, 3000);
+});
+
+firstPomise
+  .then(result => root.insertAdjacentHTML('beforeend', result))
+  .catch(err => root.insertAdjacentHTML('beforeend', err));
+
+const secondSuccess = `
+  <div
+    data-qa="notification"
+    class="success"
+  >Second promise was resolved</div>`;
+
+const secondPromise = Promise.race([
+  new Promise(resolve => {
+    document.addEventListener('mousedown', (e) => {
+      if (e.button === 1) {
+        e.preventDefault();
+      } else {
+        resolve(secondSuccess);
+      }
+    });
+  }),
+  new Promise(resolve => {
+    document.addEventListener('mousedown', (e) => {
+      if (e.button === 2) {
+        resolve(secondSuccess);
+      }
+    });
+  }),
+]);
+
+secondPromise
+  .then(result => root.insertAdjacentHTML('beforeend', result));
+
+const thirdSuccess = `
+  <div
+    data-qa="notification"
+    class="success"
+  >Third promise was resolved</div>`;
+
+const thirdPromise = Promise.all([
+  new Promise(resolve => {
+    document.addEventListener('mousedown', (e) => {
+      if (e.button === 0) {
+        resolve(thirdSuccess);
+      }
+    });
+  }),
+  new Promise(resolve => {
+    document.addEventListener('mousedown', (e) => {
+      if (e.button === 2) {
+        resolve(thirdSuccess);
+      }
+    });
+  }),
+]);
+
+thirdPromise.then(result => root
+  .insertAdjacentHTML('beforeend', result[0]));
