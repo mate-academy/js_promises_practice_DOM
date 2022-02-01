feea002d4ddc1c5ba0d98e253adbcdf83d81fc83
Daria Veretyak <dariaveretyak@gmail.com>
2022-02-01 20:54:17 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,58 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+function createNewMessage(newClass, text) {
+  body.insertAdjacentHTML('beforeend', `
+    <div class=${newClass} data-qa="notification"><p>${text}</p></div>
+  `);
+}
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener('mousedown', () => {
+    resolve();
+  });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  let leftClick = false;
+  let rightClick = false;
+
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      leftClick = true;
+    }
+
+    if (e.button === 2) {
+      rightClick = true;
+    }
+
+    if (leftClick && rightClick) {
+      resolve();
+    }
+  });
+});
+
+firstPromise
+  .then(() => {
+    createNewMessage('success', 'First promise was resolved');
+  })
+  .catch(() => {
+    createNewMessage('warning', 'First promise was rejected');
+  });
+
+secondPromise
+  .then(() => {
+    createNewMessage('success', 'Second promise was resolved');
+  });
+
+thirdPromise
+  .then(() => {
+    createNewMessage('success', 'Third promise was resolved');
+  });
