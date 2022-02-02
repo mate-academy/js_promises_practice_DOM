97a909e3da18e4305b5e372f559737aa8149d1fc
FocusHuK <kirill919191kirill@gmail.com>
2022-02-02 16:33:56 +0200

    Edited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,10 @@
 const body = document.querySelector('body');
 const div = document.createElement('div');
 
-div.dataset.qa = 'notification';
+function addElement() {
+  body.append(div);
+  div.dataset.qa = 'notification';
+}
 
 function firstPromise() {
   const resolver1 = (complete, error) => {
@@ -25,64 +28,56 @@ const first = firstPromise();
 
 first
   .then(() => {
-    body.append(div);
+    addElement();
     div.classList.add('success');
     div.textContent = 'First promise was resolved';
-
-    function secondPromise() {
-      const resolver1 = (complete) => {
-        body.addEventListener('mousedown', (e) => {
-          if (e.which === 1 || e.which === 3) {
-            complete();
-          }
-        });
-      };
-
-      return new Promise(resolver1);
-    }
-
-    const second = secondPromise();
-
-    second
-      .then(() => {
-        body.append(div);
-        div.textContent = 'Second promise was resolved';
-
-        function thirdPromiseLeft() {
-          return new Promise(resolve => {
-            body.addEventListener('mousedown', (e) => {
-              if (e.which === 1) {
-                resolve();
-              }
-            });
-          });
-        }
-
-        function thirdPromiseRight() {
-          return new Promise(resolve => {
-            body.addEventListener('mousedown', (e) => {
-              if (e.which === 3) {
-                resolve();
-              }
-            });
-          });
-        }
-
-        const thirdLeft = thirdPromiseLeft();
-        const thirdRight = thirdPromiseRight();
-
-        thirdLeft
-          .then(() => {
-            return thirdRight;
-          })
-          .then(() => {
-            body.append(div);
-            div.textContent = 'Third promise was resolved';
-          });
-      });
   })
   .catch(() => {
-    body.append(div);
+    addElement();
     div.classList.add('warning');
     div.textContent = 'First promise was rejected';
+  });
+
+function secondPromise() {
+  const resolver2 = (complete) => {
+    body.addEventListener('mousedown', (e) => {
+      if (e.which === 1 || e.which === 3) {
+        complete();
+      }
+    });
+  };
+
+  return new Promise(resolver2);
+}
+
+const second = secondPromise();
+
+second
+  .then(() => {
+    addElement();
+    div.classList.add('success');
+    div.textContent = 'Second promise was resolved';
+  });
+
+function thirdPromise(value) {
+  return new Promise(resolve => {
+    body.addEventListener('mousedown', (e) => {
+      if (e.which === value) {
+        resolve();
+      }
+    });
+  });
+}
+
+const thirdPromiseLeft = thirdPromise(1);
+const thirdPromiseRight = thirdPromise(3);
+
+thirdPromiseLeft
+  .then(() => {
+    return thirdPromiseRight;
+  })
+  .then(() => {
+    addElement();
+    div.classList.add('success');
+    div.textContent = 'Third promise was resolved';
   });
