61898a62d49577acce3b44d219207e58dac5ace4
KristinaTar <kristina.tarashchuk@gmail.com>
2022-03-19 18:35:22 +0200

    version1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,65 @@
 'use strict';
+/* eslint-disable */
+
+const body = document.querySelector('body');
+
+const firstPromise = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      reject('First promise was rejected');
+    }, 3000)
+    body.addEventListener('click', () => {
+      resolve('First promise was resolved');
+    });
+});
+
+function successHandler(message) {
+    const div = document.createElement('div');
+    div.setAttribute("data-qa", "notification");
+    div.classList.add('success');
+    div.innerText = message;
+    body.append(div);
+}
+
+function errorHandler(message) {
+    const div = document.createElement('div');
+    div.setAttribute("data-qa", "notification");
+    div.classList.add('warning');
+    div.innerText = message;
+    body.append(div);
+}
+
+firstPromise.then(successHandler);
+
+firstPromise.catch(errorHandler);
+
+const secondPromise = new Promise((resolve) => {
+    body.addEventListener('click', () => {
+      resolve('Second promise was resolved');
+    });
+    body.addEventListener('contextmenu', () => {
+        resolve('Second promise was resolved');
+    });
+    
+});
+
+secondPromise.then(successHandler);
+
+const thirdPromise = new Promise((resolve) => {
+    let counterR = false;
+    let counterL = false;
+    body.addEventListener('click', () => {
+        counterR = true;
+        if(counterR === true && counterL === true) {
+            resolve('Third promise was resolved');
+        }
+    });
+    body.addEventListener('contextmenu', () => {
+        counterL= true ;
+        if(counterR === true && counterL === true) {
+            resolve('Third promise was resolved');
+        }
+    });
+
+});
+
+thirdPromise.then(successHandler);
