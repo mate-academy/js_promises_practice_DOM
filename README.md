6b763a458bbfd8ad75208f5cdc4382b9fae13f5e
Alina Yermolenko <alina.yermolenko27@gmail.com>
2022-06-17 13:12:12 +0300

    done

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Create 3 promises:
 - The `firstPromise` should be
   - **resolved** with a message `First promise was resolved` on a left `click` in the `document`
-  - **rejected** with a message `First promise was rejected` in 3 seconds if not clicked
+  - **rejecp;o[;[pted** with a message `First promise was rejected` in 3 seconds if not clicked
 - The `secondPromise` should be:
   - **resolved** only on `left` or `right` click in the `document` with a message `Second promise was resolved`
   - never **rejected**
@@ -12,7 +12,7 @@ Add `success` and `error` handlers to each promise to show `<div data-qa="notifi
 
 ## Instructions
 1. Replace `<your_account>` with your Github username in the link
-    - [DEMO LINK](https://<your_account>.github.io/js_promises_practice_DOM/)
+    - [DEMO LINK](https://Alina-Yermolenko.github.io/js_promises_practice_DOM/)
 2. Follow [this instructions](https://mate-academy.github.io/layout_task-guideline/)
     - Run `npm run test` command to test your code;
     - Run `npm run test:only -- -n` to run fast test ignoring linter;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,104 @@
 'use strict';
+
+function promise1() {
+  const resolver = (confirm1, reject1) => {
+    document.addEventListener('contextmenu', () => {
+      return confirm1('First promise was resolved');
+    });
+
+    setTimeout(() => {
+      return reject1('First promise was rejected');
+    }, 3000);
+  };
+
+  return new Promise(resolver);
+}
+
+promise1()
+  .then(result => {
+    const div = document.querySelector('body');
+
+    div.innerHTML
+    += `<div class="success"
+     data-qa="notification">
+     ${result}</div>`;
+  })
+  .catch(error => {
+    const div = document.querySelector('body');
+
+    div.innerHTML
+    += `<div class="warning" 
+    data-qa="notification">
+    ${error}</div>`;
+  });
+
+function promise2() {
+  let left = 0;
+  let right = 0;
+  const resolved = (confirm2, reject2) => {
+    document.addEventListener('click', (e) => {
+      e.preventDefault();
+      left++;
+
+      if (left > 0 || right > 0) {
+        confirm2();
+      }
+    });
+
+    document.addEventListener('contextmenu', () => {
+      right++;
+
+      if (left > 0 || right > 0) {
+        confirm2();
+      }
+    });
+  };
+
+  return new Promise(resolved);
+}
+
+promise2()
+  .then(result => {
+    const div = document.querySelector('body');
+
+    div.innerHTML += `<div 
+    class="success" 
+    data-qa="notification">
+    Second promise was resolved</div>`;
+  });
+
+function promise3() {
+  let left = 0;
+  let right = 0;
+  const resolved = (confirm3, reject) => {
+    document.addEventListener('click', () => {
+      left++;
+
+      if (left > 0 && right > 0) {
+        confirm3();
+      }
+    });
+
+    document.addEventListener('contextmenu', (e) => {
+      right++;
+      e.preventDefault();
+
+      if (left > 0 && right > 0) {
+        confirm3();
+      }
+    });
+  };
+
+  return new Promise(resolved);
+}
+
+promise3()
+  .then(result => {
+    const div = document.querySelector('body');
+
+    div.innerHTML
+    += `<div class="success" 
+    data-qa="notification">
+    Third promise was resolved
+    </div>`;
+  });
