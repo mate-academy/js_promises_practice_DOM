f4bd6de1a2245ba5cec2c4af9d262e9bc85ca153
Kostiantyn <havryshkostya@gmail.com>
2022-06-27 19:04:19 +0400

    looks like that

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,57 @@
 'use strict';
 
-let documentClicked = false;
+let countRight = 0;
+let countLeft = 0;
 
 const promise1 = new Promise((resolve, reject) => {
   document.addEventListener('click', e => {
     resolve();
-    documentClicked = true;
+    countLeft++;
   });
 
-  if (documentClicked === false) {
+  if (countLeft === 0) {
     setTimeout(() => reject(new Error()), 3000);
   }
 });
 
-promise1
-  .then(document.body.insertAdjacentHTML('beforeend', `
-      <div data-qa="notification" class="success">First promise was resolved</div>
-  `))
-  .catch(() => {
-    document.body.insertAdjacentHTML('beforeend', `
-      <div data-qa="notification" class="warning">First promise was rejected</div>
-    `);
+const promise2 = new Promise((resolve) => {
+  document.addEventListener('click', resolve);
+  document.addEventListener('contextmenu', resolve);
+});
+
+const promise3 = new Promise((resolve) => {
+  document.addEventListener('click', e => {
+    countLeft = 0;
+    countLeft++;
+
+    if (countLeft === 1 && countRight === 1) {
+      resolve();
+    }
   });
+
+  document.addEventListener('contextmenu', e => {
+    countRight++;
+
+    if (countLeft === 1 && countRight === 1) {
+      resolve();
+    }
+  });
+});
+
+function createResponse(className, promiseNumber, result) {
+  document.body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification" class="${className}">
+      ${promiseNumber} promise was ${result}
+    </div>
+  `);
+}
+
+promise1
+  .then(() => createResponse('success', 'First', 'resolved'))
+  .catch(() => createResponse('warning', 'First', 'rejected'));
+
+promise2
+  .then(() => createResponse('success', 'Second', 'resolved'));
+
+promise3
+  .then(() => createResponse('success', 'Third', 'resolved'));
