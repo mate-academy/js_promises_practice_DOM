3f364a7cfcd4382bd23932d187e2e3beec0c2597
Kostiantyn <havryshkostya@gmail.com>
2022-06-27 18:29:18 +0400

    check the addEL

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+let documentClicked = false;
+
+const promise1 = new Promise((resolve, reject) => {
+  document.addEventListener('click', e => {
+    resolve();
+    documentClicked = true;
+  });
+
+  if (documentClicked === false) {
+    setTimeout(() => reject(new Error()), 3000);
+  }
+});
+
+promise1
+  .then(document.body.insertAdjacentHTML('beforeend', `
+      <div data-qa="notification" class="success">First promise was resolved</div>
+  `))
+  .catch(() => {
+    document.body.insertAdjacentHTML('beforeend', `
+      <div data-qa="notification" class="warning">First promise was rejected</div>
+    `);
+  });
