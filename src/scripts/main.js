5659b3846646cfb64ee5f5df845e1e0ad3c9ca23
Mikhail Shchuka <mikhail.shchuka@gmail.com>
2022-06-28 21:57:47 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,47 @@
 'use strict';
+
+const leftClick = new Promise((resolve) => {
+  document.addEventListener('click', () => resolve());
+});
+const rightClick = new Promise((resolve) => {
+  document.addEventListener('contextmenu', () => resolve());
+});
+
+const firstPromise = new Promise((resolve, reject) => {
+  leftClick.then(() => resolve('First promise was resolved'));
+
+  setTimeout(() => reject(new Error('First promise was rejected')), 3000);
+});
+
+const secondPromise = new Promise((resolve) => {
+  leftClick.then(() => resolve('Second promise was resolved'));
+  rightClick.then(() => resolve('Second promise was resolved'));
+});
+
+const thirdPromise = new Promise((resolve) => {
+  leftClick.then(() =>
+    rightClick.then(() =>
+      resolve('Third promise was resolved')));
+});
+
+function handler(className) {
+  return function(message) {
+    document.body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification" class="${className}">
+      ${message}
+    </div>
+    `);
+  };
+}
+
+firstPromise
+  .then(handler('success'))
+  .catch(handler('warning'));
+
+secondPromise
+  .then(handler('success'))
+  .catch(handler('warning'));
+
+thirdPromise
+  .then(handler('success'))
+  .catch(handler('warning'));
