9bf58b2a88ba49210d3399f1fcd518484def465a
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-07-13 14:36:12 +0300

    firstCommit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,59 @@
 'use strict';
+
+const body = document.body;
+
+const message = (type, text) => {
+  const div = `<div data-qa="notification" class="${type}">${text}</div>`;
+
+  body.insertAdjacentHTML('beforeend', div);
+};
+
+const promise1 = () => {
+  return new Promise((resolve, reject) => {
+    document.body.addEventListener('click', () => {
+      resolve('First promise was resolved');
+    });
+
+    setTimeout(() => {
+      reject(new Error('First promise was rejected'));
+    }, 3000);
+  });
+};
+
+const promise2 = () => {
+  return new Promise((resolve) => {
+    document.body.addEventListener('mousedown', (e) => {
+      if (e.button === 0 || e.button === 2) {
+        resolve('Second promise was resolved');
+      }
+    });
+  });
+};
+
+const promise3 = () => {
+  return new Promise((resolve) => {
+    document.body.addEventListener('mousedown', (e) => {
+      if (e.button === 0 && e.button === 2) {
+        resolve('Third promise was resolved');
+      }
+    });
+  });
+};
+
+promise1()
+  .then(result => {
+    message('success', result);
+  })
+  .catch(error => {
+    message('warning', error);
+  });
+
+promise2()
+  .then(result => {
+    message('success2', result);
+  });
+
+promise3()
+  .then(result => {
+    message('success2', result);
+  });
