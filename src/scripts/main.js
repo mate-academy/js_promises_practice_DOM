655bf770f44887ad92dd5b91004e143e7e29add8
Vitaliy Anochin <anuhin.vital@gmail.com>
2022-07-16 14:52:05 +0300

    message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,68 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+const firstPromise = new Promise((resolve, reject) => {
+  body.addEventListener('mousedown', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  });
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  body.addEventListener((element) => {
+    if (element.button === 0 || element.button === 2) {
+      resolve('Second promise was resolved');
+    }
+  });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  body.addEventListener('mousedown', (element) => {
+    if (element.button === 0) {
+      body.addEventListener('mousedown', (el) => {
+        if (el.button === 2) {
+          resolve('Third promise was resolved');
+        }
+      });
+    }
+
+    if (element.button === 2) {
+      body.addEventListener('mousedown', (el) => {
+        if (el.button === 0) {
+          resolve('Third promise was resolved');
+        }
+      });
+    }
+  });
+});
+
+function onSuccess(message) {
+  body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification" class="success">
+      ${message}
+    </div>
+  `);
+}
+
+function onWarning(message) {
+  body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification" class="warning">
+      ${message}
+    </div>
+  `);
+}
+
+firstPromise
+  .then(onSuccess)
+  .catch(onWarning);
+
+secondPromise
+  .then(onSuccess)
+  .then(onSuccess);
+
+thirdPromise
+  .then(onSuccess);
