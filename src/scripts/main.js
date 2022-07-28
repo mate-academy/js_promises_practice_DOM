da4dc8caa0e49524f004ef213c3eea3b883d1f72
Aleksandr Kovalenko <dias.koval@gmail.com>
2022-07-28 19:46:41 +0300

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,56 @@
 'use strict';
+
+function notification(className, text) {
+  document.body.insertAdjacentHTML('afterbegin', `
+  <div class="${className}" data-qa="notification">
+  ${text}
+</div>
+  `);
+}
+
+const promise1 = (resolve, reject) => {
+  document.addEventListener('mousedown', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+};
+
+promise1
+  .then(result => notification(result, 'success'))
+  .catch(error => notification(error, 'warning'));
+
+const promise2 = (resolve) => {
+  document.addEventListener('mousedown', e => {
+    if (e.button === 0 || e.button === 2) {
+      resolve('Second promise was resolved');
+    };
+  });
+};
+
+promise2
+  .then(result => notification(result, 'success'));
+
+const promise3 = (resolve) => {
+  let lclick = false;
+  let rclick = false;
+
+  document.addEventListener('mousedown', e => {
+    if (e.button === 0) {
+      lclick = true;
+    };
+
+    if (e.button === 2) {
+      rclick = true;
+    };
+
+    if (lclick && rclick) {
+      resolve('Third promise was resolved');
+    }
+  });
+};
+
+promise3
+  .then(result => notification(result, 'success'));
