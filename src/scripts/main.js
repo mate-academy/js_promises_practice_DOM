4c9a1c020abee1e56bed803b95bffd05fbb7772e
Aleksandr Kovalenko <dias.koval@gmail.com>
2022-08-07 21:23:45 +0300

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,7 +8,7 @@ function notification(className, text) {
   `);
 }
 
-const promise1 = (resolve, reject) => {
+const promise1 = new Promise((resolve, reject) => {
   document.addEventListener('mousedown', () => {
     resolve('First promise was resolved');
   });
@@ -16,24 +16,24 @@ const promise1 = (resolve, reject) => {
   setTimeout(() => {
     reject(new Error('First promise was rejected'));
   }, 3000);
-};
+});
 
 promise1
   .then(result => notification(result, 'success'))
   .catch(error => notification(error, 'warning'));
 
-const promise2 = (resolve) => {
+const promise2 = new Promise((resolve) => {
   document.addEventListener('mousedown', e => {
     if (e.button === 0 || e.button === 2) {
       resolve('Second promise was resolved');
     };
   });
-};
+});
 
 promise2
   .then(result => notification(result, 'success'));
 
-const promise3 = (resolve) => {
+const promise3 = new Promise((resolve) => {
   let lclick = false;
   let rclick = false;
 
@@ -50,7 +50,7 @@ const promise3 = (resolve) => {
       resolve('Third promise was resolved');
     }
   });
-};
+});
 
 promise3
   .then(result => notification(result, 'success'));
