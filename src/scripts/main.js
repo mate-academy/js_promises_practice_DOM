93c71a188281909a0b4eb3177e7a215efec13bf0
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2022-08-10 09:50:19 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,81 @@
 'use strict';
+
+function appendDiv(res, type) {
+  document.body.insertAdjacentHTML('beforeend', `
+  <div data-qa="notification" class="${type}">
+    ${res}
+  </div>
+    `)
+}
+
+//—————————————————FIRST PROMISE—————————————————
+
+let isFirstPromiseResolved = false;
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve(`First promise was resolved`);
+    isFirstPromiseResolved = true;
+  })
+
+  setTimeout(() => {
+    if (!isFirstPromiseResolved) {
+      reject('First promise was rejected')
+      console.log(firstPromise);
+    }
+  }, 3000)
+})
+  .then((res) => {
+    appendDiv(res, 'success')
+  })
+  .catch(res => {
+    appendDiv(res, 'warning')
+  })
+
+//—————————————————SECOND PROMISE—————————————————
+
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  })
+
+  document.addEventListener('contextmenu', () => {
+    resolve('Second promise was resolved');
+  })
+})
+  .then(res => {
+    appendDiv(res, 'success')
+  })
+  .catch(res => {
+    appendDiv(res, 'warning')
+  })
+
+//—————————————————THIRD PROMISE—————————————————
+
+let isLeftClicked = false;
+let isRightClicked = false;
+
+const thirdPromise = new Promise((resolve) => {
+  document.addEventListener('click', () => {
+    if (isRightClicked) {
+      resolve('Third promise was resolved');
+    }
+
+    isLeftClicked = true;
+  })
+
+  document.addEventListener('contextmenu', () => {
+    if (isLeftClicked) {
+      resolve('Third promise was resolved');
+    }
+
+    isRightClicked = true;
+  })
+
+  
+})
+  .then(res => {
+    appendDiv(res, 'success')
+  })
+  .catch(res => {
+    appendDiv(res, 'warning')
+  })
