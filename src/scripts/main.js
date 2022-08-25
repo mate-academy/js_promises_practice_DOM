cf1f34c55bfe4d9b7a99fc73ae77a13062cfc63f
Anton Novyk <Novyk.anton@gmail.com>
2022-08-25 16:57:16 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,74 @@
 'use strict';
+
+const doc = document.querySelector('body');
+
+const promise1 = new Promise((resolve, reject) => {
+  const click = doc.addEventListener('click', (e) =>
+    resolve('First promise was resolved'));
+
+  setTimeout(() => {
+    if (!click) {
+      reject(new Error('First promise was rejected'));
+    }
+  }, 3000);
+});
+
+promise1
+  .then(result => {
+    doc.insertAdjacentHTML('beforeend',
+      `<div data-qa="notification" class="success">
+      First promise was resolved</div>`);
+  })
+
+  .catch(er => {
+    doc.insertAdjacentHTML('beforeend',
+      `<div data-qa="notification" class="warning">
+      First promise was rejected</div>`);
+  });
+
+const promise2 = new Promise((resolve) => {
+  doc.addEventListener('click', () =>
+    resolve('First promise was resolved'));
+
+  doc.addEventListener('contextmenu', () =>
+    resolve('First promise was resolved'));
+});
+
+promise2
+  .then(result => doc.insertAdjacentHTML('beforeend',
+    `<div data-qa="notification" class="success">
+    Second promise was resolved</div>`))
+
+  .catch(er => doc.insertAdjacentHTML('beforeend',
+    `<div data-qa="notification" class="warning">
+    Second promise was rejected</div>`));
+
+let leftClick = false;
+let rightClick = false;
+
+const promise3 = new Promise((resolve, reject) => {
+  doc.addEventListener('click', () => {
+    leftClick = true;
+
+    if (leftClick === true && rightClick === true) {
+      resolve('Third promise was resolved');
+    }
+  });
+
+  doc.addEventListener('contextmenu', () => {
+    rightClick = true;
+
+    if (leftClick === true && rightClick === true) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+promise3
+  .then(result => doc.insertAdjacentHTML('beforeend',
+    `<div data-qa="notification" class="success">
+    Third promise was resolved</div>`))
+
+  .catch(er => doc.insertAdjacentHTML('beforeend',
+    `<div data-qa="notification" class="warning">
+    Third promise was rejected</div>`));
