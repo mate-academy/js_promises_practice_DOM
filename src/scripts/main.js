d7b812822891a3294ab3a5bbd30f48b3456c9caf
Viktoria Neimesh <valencia88vika@gmail.com>
2022-08-31 14:41:19 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,68 @@
 'use strict';
+
+let rightClick;
+let leftClick;
+
+function showMessage(nameClass, message) {
+  const div = document.createElement('div');
+
+  div.setAttribute('data-qa', 'notification');
+
+  div.classList.add(nameClass);
+  div.innerHTML = message;
+  document.body.append(div);
+}
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener('mousedown', (e) => {
+    if (e.button !== 1) {
+      resolve('Second promise was resolved');
+    }
+  });
+});
+
+const thirdPromise = new Promise((resolve) => {
+  document.addEventListener('mousedown', (e) => {
+    switch (e.button) {
+      case 0:
+        leftClick = true;
+        break;
+
+      case 2:
+        rightClick = true;
+        break;
+    }
+
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+firstPromise
+  .then(message => {
+    showMessage('success', message);
+  })
+  .catch(message => {
+    showMessage('warning', message);
+  });
+
+secondPromise
+  .then(message => {
+    showMessage('success', message);
+  });
+
+thirdPromise
+  .then(message => {
+    showMessage('success', message);
+  });
