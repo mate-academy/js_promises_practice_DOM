73bbb1f76ddb89e00eae3e5e006a238d4b40b8f4
Oleksandr Malyshev <sanyokmalyshev@gmail.com>
2022-09-12 19:14:10 +0800

    Task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,64 @@
 'use strict';
+
+const body = document.body;
+
+const promise1 = new Promise((resolve, reject) => {
+  body.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  body.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  });
+
+  body.addEventListener('contextmenu', () => {
+    resolve('Second promise was resolved');
+  });
+});
+
+const promise3 = new Promise((resolve) => {
+  let leftClick;
+  let rightClick;
+
+  body.addEventListener('click', () => {
+    leftClick = true;
+
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+
+  body.addEventListener('contextmenu', () => {
+    rightClick = true;
+
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+function success(message) {
+  body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification" class="success">
+      ${message}
+    </div>
+  `);
+};
+
+function error(message) {
+  body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification" class="warning">
+      ${message}
+    </div>
+  `);
+};
+
+promise1.then(success, error);
+promise2.then(success);
+promise3.then(success);
