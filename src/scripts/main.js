67e9d31b44b63a7106b508a4df82c256db16fb2e
Boris Kerekesha <kerekesha90@ukr.net>
2022-09-27 16:06:17 +0300

    practice_promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,77 @@
 'use strict';
+
+let leftClick = '';
+let rightClick = '';
+
+const resolveMessage = (text) => {
+  document.body.insertAdjacentHTML('afterbegin', `
+    <div class="success" data-qa="notification">
+      ${text}
+    </div>
+  `);
+};
+const rejectedMessage = (text) => {
+  document.body.insertAdjacentHTML('afterbegin', `
+  <div class="warning" data-qa="notification">
+    ${text}
+  </div>
+`);
+};
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.body.addEventListener('click', (evt) => {
+    if (evt.hasClickedLeft) {
+      return;
+    }
+    leftClick = true;
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    if (leftClick !== true) {
+      reject(new Error('First promise was rejected'));
+    }
+  }, 3000);
+});
+
+const secondPromise = new Promise((resolve) => {
+  document.body.addEventListener('click', (evt) => {
+    leftClick = true;
+    resolve('Second promise was resolved');
+  });
+
+  document.body.addEventListener('contextmenu', (evt) => {
+    evt.preventDefault();
+    rightClick = true;
+    resolve('Second promise was resolved');
+  });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  document.body.addEventListener('click', (evt) => {
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+
+  document.body.addEventListener('contextmenu', (evt) => {
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+firstPromise.then(
+  () => resolveMessage('First promise was resolved')
+)
+  .catch(
+    () => rejectedMessage('First promise was rejected')
+  );
+
+secondPromise.then(
+  () => resolveMessage('Second promise was resolved')
+);
+
+thirdPromise.then(
+  () => resolveMessage('Third promise was resolved')
+);
