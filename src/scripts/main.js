eed0721506d83a618d9f4a0ad4234f3b6f42437d
Paul Voronin <voron.paul@gmail.com>
2022-09-27 11:17:08 +0300

    Solved. V.1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,78 @@
 'use strict';
+
+const body = document.querySelector('body');
+let isLeftClicked = false;
+let isRightClicked = false;
+
+const firstPromise = new Promise((resolve, reject) => {
+  body.addEventListener('click', () => {
+    resolve();
+  });
+  setTimeout(() => reject(new Error()), 3000);
+});
+
+const secondPromise = new Promise((resolve) => {
+  body.addEventListener('click', () => {
+    resolve();
+  });
+
+  body.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    resolve();
+  });
+});
+
+const thirdPromise = new Promise((resolve) => {
+  body.addEventListener('click', () => {
+    isLeftClicked = true;
+
+    if (isLeftClicked && isRightClicked) {
+      resolve();
+    }
+  });
+
+  body.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    isRightClicked = true;
+
+    if (isLeftClicked && isRightClicked) {
+      resolve();
+    }
+  });
+});
+
+firstPromise
+  .then(result => {
+    resolved('First');
+  })
+  .catch(er => {
+    rejected('First');
+  });
+
+secondPromise
+  .then(result => {
+    resolved('Second');
+  })
+  .catch(er => {
+    rejected('Second');
+  });
+
+thirdPromise
+  .then(result => {
+    resolved('Third');
+  })
+  .catch(er => {
+    rejected('Third');
+  });
+
+function resolved(n) {
+  body.insertAdjacentHTML('beforeend',
+    `<div data-qa="notification" class="success">
+    ${n} promise was resolved</div>`);
+}
+
+function rejected(n) {
+  body.insertAdjacentHTML('beforeend',
+    `<div data-qa="notification" class="warning">
+    ${n} promise was rejected</div>`);
+}
