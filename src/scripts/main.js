e6865e30125539a0e78584f553006002b41047c8
Dima <dimak119988@gmail.com>
2022-10-07 20:20:10 +0300

    First Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,60 @@
 'use strict';
+
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve('success');
+  });
+
+  setTimeout(() => reject(new Error('error').message), 3000);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve('success');
+  });
+
+  logo.addEventListener('contextmenu', () => {
+    event.preventDefault();
+    resolve('success');
+  });
+});
+
+const promise3 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    logo.addEventListener('contextmenu', () => {
+      resolve('success');
+    });
+  });
+
+  logo.addEventListener('contextmenu', () => {
+    logo.addEventListener('click', () => {
+      resolve('success');
+    });
+  });
+});
+
+const notifApender = (result, nameOfPromise) => {
+  const notifResult = result === 'success'
+    ? 'resolved'
+    : 'rejected';
+  const message = `
+    <div data-qa="notification">
+      ${nameOfPromise} promise was ${notifResult}
+    </div>
+  `;
+
+  body.insertAdjacentHTML('afterbegin', message);
+};
+
+promise1
+  .then(result => notifApender(result, 'First'))
+  .catch(result => notifApender(result, 'First'));
+
+promise2
+  .then(result => notifApender(result, 'Second'));
+
+promise3
+  .then(result => notifApender(result, 'Third'));
