30056eea3376a450aab55a60619b277d7a431a95
Vitalii Babiienko <v.babiyenko@gmail.com>
2022-10-08 17:09:53 +0300

    solution improved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,56 +13,41 @@ const firstPromise = new Promise((resolve, reject) => {
 });
 
 const secondPromise = new Promise((resolve, reject) => {
-  body.addEventListener('mousedown', e => {
-    if (e.button === 0 || e.button === 2) {
-      resolve('Second promise was resolved');
-    }
+  body.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  });
+
+  body.addEventListener('contextmenu', e => {
+    e.preventDefault();
+
+    resolve('Second promise was resolved');
   });
 });
 
 const thirdPromise = new Promise((resolve, reject) => {
-  let leftClick = false;
-  let rightClick = false;
-
-  body.addEventListener('mousedown', e => {
-    if (e.button === 0) {
-      leftClick = true;
-    }
-
-    if (e.button === 2) {
-      rightClick = true;
-    }
-
-    if (leftClick && rightClick) {
+  body.addEventListener('click', () => {
+    body.addEventListener('contextmenu', () => {
       resolve('Third promise was resolved');
-    }
+    });
   });
 });
 
-function success(message) {
+function showMessage(message, className) {
   body.insertAdjacentHTML('beforeend', `
-    <div class="success" data-qa="notification">
-      ${message}
-    </div>
-  `);
-}
-
-function error(message) {
-  body.insertAdjacentHTML('beforeend', `
-    <div class="warning" data-qa="notification">
+    <div class="${className}" data-qa="notification">
       ${message}
     </div>
   `);
 }
 
 firstPromise
-  .then(success)
-  .catch(error);
+  .then(success => showMessage(success, 'message success'))
+  .catch(error => showMessage(error, 'message warning'));
 
 secondPromise
-  .then(success)
-  .catch(error);
+  .then(success => showMessage(success, 'message success'))
+  .catch(error => showMessage(error, 'message warning'));
 
 thirdPromise
-  .then(success)
-  .catch(error);
+  .then(success => showMessage(success, 'message success'))
+  .catch(error => showMessage(error, 'message warning'));
