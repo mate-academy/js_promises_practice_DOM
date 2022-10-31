9be2f5a09f1f0fa2a530cf63442e2f2dc14dac3c
Vitaliy Bushovskiy <Bushovskiy@gmail.com>
2022-10-31 13:26:21 +0200

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,47 @@
 'use strict';
+
+const promise1 = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+const promise2 = new Promise((resolve) => {
+  document.addEventListener('mousedown', () => {
+    resolve('Second promise was resolved');
+  });
+});
+
+const promise3 = new Promise((resolve) => {
+  document.addEventListener('click' && 'contextmenu', () => {
+    resolve('Third promise was resolved');
+  });
+});
+
+function appendDiv(res, type) {
+  document.body.insertAdjacentHTML('beforeend', ` 
+    <div data-qa="notification" class="${type}"> ${res}  </div>`
+  );
+}
+
+promise1
+  .then((res) => {
+    appendDiv(res, 'success success__1');
+  })
+  .catch((res) => {
+    appendDiv(res, 'warning');
+  });
+
+promise2
+  .then((res) => {
+    appendDiv(res, 'success success__2');
+  });
+
+promise3
+  .then((res) => {
+    appendDiv(res, 'success success__3');
+  });
