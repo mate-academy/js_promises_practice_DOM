498213abde613f906bc1e555328d9c60101660df
Vlad Malinin <vladmalinin1999@gmail.com>
2022-11-02 16:13:46 +0200

    Promises practice

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,74 @@
 'use strict';
+
+const appendDivToPage = (type, message) => {
+  const div = document.createElement('div');
+
+  div.classList.add(`${type}`);
+  div.setAttribute('data-qa', 'notification');
+  div.innerText = message;
+  document.body.appendChild(div);
+};
+
+const promise1 = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+const promise2 = new Promise(resolve => {
+  document.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  });
+
+  document.addEventListener('contextmenu', () => {
+    resolve('Second promise was resolved');
+  });
+});
+
+const promise3 = new Promise((resolve) => {
+  document.addEventListener('click', () => {
+    resolve('left click');
+  });
+
+  document.addEventListener('contextmenu', () => {
+    resolve('right click');
+  });
+});
+
+promise1
+  .then((result) => {
+    appendDivToPage('success', result);
+  })
+  .catch((result) => {
+    appendDivToPage('warning', result);
+  });
+
+promise2
+  .then((result) => {
+    appendDivToPage('success', result);
+  });
+
+promise3
+  .then((result) => {
+    let listener;
+
+    if (result === 'left click') {
+      listener = 'contextmenu';
+    } else {
+      listener = 'click';
+    }
+
+    return new Promise(resolve => {
+      document.addEventListener(listener, (e) => {
+        e.preventDefault();
+        resolve('Third promise was resolved');
+      });
+    });
+  })
+  .then((result) => {
+    appendDivToPage('success', result);
+  });
