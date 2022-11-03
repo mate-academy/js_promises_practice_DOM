fc8ebb10166b80e5e20e65785b72b09c163ebd13
Anton Martyniuk <antonmartyniuk@gmail.com>
2022-11-03 21:56:52 +0200

    Solution js_promises_practice_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,59 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+const resolver1 = (resolv, reject) => {
+  document.addEventListener('click', () => {
+    resolv();
+  });
+
+  setTimeout(() => {
+    reject();
+  }, 3000);
+};
+
+const resolver2 = (resolv) => {
+  document.addEventListener('click', () => {
+    resolv();
+  });
+
+  document.addEventListener('contextmenu', () => {
+    resolv();
+  });
+};
+
+const promise1 = new Promise(resolver1);
+const promise2 = new Promise(resolver2);
+const promise3 = Promise.all([promise1, promise2]);
+
+promise1
+  .then(result => {
+    body.insertAdjacentHTML('afterbegin', `
+      <div class="success" data-qa="notification">
+        First promise was resolved
+      </div>
+    `);
+  })
+  .catch(er => {
+    body.insertAdjacentHTML('afterbegin', `
+      <div class="error" data-qa="notification">
+        First promise was rejected
+      </div>
+    `);
+  });
+
+promise2.then(result => {
+  body.insertAdjacentHTML('afterbegin', `
+    <div class="success" data-qa="notification">
+      Second promise was resolved
+    </div>
+  `);
+});
+
+promise3.then(result => {
+  body.insertAdjacentHTML('afterbegin', `
+    <div class="success" data-qa="notification">
+      Third promise was resolved
+    </div>
+  `);
+});
