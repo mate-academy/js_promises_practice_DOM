8040468dbf1a4210f4b2abf41a6d9803a1e7ad60
Vitalii Derlemenko <vitaliidox@gmail.com>
2022-11-07 11:16:31 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,59 @@
 'use strict';
+
+const promise1 = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+})
+  .then(data => {
+    document.body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification" class="success">
+      ${data}
+    </div>
+    `);
+  })
+  .catch(err => {
+    document.body.innerHTML += `
+    <div data-qa="notification" class="warning">
+      ${err}
+    </div>
+    `;
+  });
+
+const promise2 = new Promise((resolve) => {
+  ['click', 'contextmenu'].forEach(handler => {
+    document.addEventListener(handler, () => {
+      resolve('Second promise was resolved');
+    });
+  });
+})
+  .then(data => {
+    document.body.innerHTML += `
+    <div data-qa="notification" class="success">
+      ${data}
+    </div>
+    `;
+  });
+
+const promise3 = Promise.all([
+  new Promise((resolve) => {
+    document.addEventListener('click', () => {
+      resolve('Third promise was resolved');
+    });
+  }),
+  new Promise((resolve) => {
+    document.addEventListener('contextmenu', () => {
+      resolve();
+    });
+  })])
+  .then(data => {
+    document.body.innerHTML += `
+    <div data-qa="notification" class="success">
+      ${data}
+    </div>
+    `;
+  });
