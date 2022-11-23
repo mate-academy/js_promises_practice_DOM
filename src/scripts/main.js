69d97c81e6480b4f59e360e61595db14531a5d9b
Mykyta Krainik <nikinta2003@gmail.com>
2022-11-23 13:13:08 +0200

    Empty lines between declarations were added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,23 +9,28 @@ const firstPromise = new Promise((resolve, reject) => {
     reject(new Error('First promise was rejected'));
   }, 3000);
 });
+
 const secondPromise = new Promise(resolve => {
   const clickHandler = () => resolve('Second promise was resolved');
 
   document.addEventListener('click', clickHandler);
   document.addEventListener('contextmenu', clickHandler);
 });
+
 const leftClick = new Promise(resolve => {
   document.addEventListener('click', () => resolve());
 });
+
 const rightClick = new Promise(resolve => {
   document.addEventListener('contextmenu', () => resolve());
 });
+
 const thirdPromise = new Promise(resolve => {
   Promise
     .all([leftClick, rightClick])
     .then(() => resolve('Third promise was resolved'));
 });
+
 const appendMessage = (message, type) => {
   document.body.insertAdjacentHTML('beforeend', `
     <div class="${type}" data-qa="notification">
