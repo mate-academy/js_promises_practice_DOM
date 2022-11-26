2f75703c86d2b2cb1aeed5de2ac5b72232d3f5ee
Denis Cherevaschenko <deniska.cherevaschenko@gmail.com>
2022-11-26 21:27:37 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,57 @@
 'use strict';
+
+const root = document.body;
+
+// firstPromise
+new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve();
+  });
+
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+}).then(() => {
+  root.insertAdjacentHTML('beforeend', `
+    <div data-qa="success">First promise was resolved</div>
+  `);
+}).catch(() => {
+  root.insertAdjacentHTML('beforeend', `
+    <div data-qa="error">First promise was rejected</div>
+  `);
+});
+
+// secondPromise
+new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve();
+  });
+
+  document.addEventListener('contextmenu', e => {
+    e.preventDefault();
+    resolve();
+  });
+}).then(() => {
+  root.insertAdjacentHTML('beforeend', `
+    <div data-qa="success">Second promise was resolved</div>
+  `);
+});
+
+// thirdPromise
+new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    document.addEventListener('contextmenu', () => {
+      resolve();
+    });
+  });
+
+  document.addEventListener('contextmenu', () => {
+    document.addEventListener('click', () => {
+      resolve();
+    });
+  });
+}).then(() => {
+  root.insertAdjacentHTML('beforeend', `
+    <div data-qa="success">Third promise was resolved</div>
+  `);
+});
