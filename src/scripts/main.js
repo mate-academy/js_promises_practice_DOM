6481f6ee739c2704879d2017a96beb28b26d8052
Antonenko-AA <alexandr.antonenko.fr@gmail.com>
2022-11-29 04:16:25 +0200

    Promises practice has been solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,71 @@
 'use strict';
+
+const notifications = document.createElement('div');
+
+notifications.classList.add('notifications');
+document.body.appendChild(notifications);
+
+const generateNotification = (className, message) => {
+  notifications.insertAdjacentHTML('beforeend', `
+    <div
+      class="notification ${className}"
+      data-qa="notification">${message}
+    </div>
+  `);
+};
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+const secondPromise = new Promise((resolve) => {
+  let leftClick, rightClick;
+
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      leftClick = true;
+    }
+
+    if (e.button === 2) {
+      rightClick = true;
+    }
+
+    if (leftClick || rightClick) {
+      resolve('Second promise was resolved');
+    }
+  });
+});
+
+const thirdPromise = new Promise((resolve) => {
+  let leftClick, rightClick;
+
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      leftClick = true;
+    }
+
+    if (e.button === 2) {
+      rightClick = true;
+    }
+
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+firstPromise
+  .then(data => generateNotification('success', data))
+  .catch(error => generateNotification('warning', error));
+
+secondPromise
+  .then(data => generateNotification('success', data));
+
+thirdPromise
+  .then(data => generateNotification('success', data));
