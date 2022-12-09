1560f6f1c9d95d1b8c7d5949548317959df5e57f
Illia Kyselov <illya.kis99@gmail.com>
2022-12-09 19:06:17 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,52 @@
 'use strict';
+
+const promise1 = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+promise1
+  .then(message => addMessage('success', message))
+  .catch(message => addMessage('warning', message));
+
+const promise2 = new Promise((resolve, reject) => {
+  document.addEventListener('click', (e) => {
+    if (e.button === 0 || e.button === 2) {
+      resolve('Second promise was resolved');
+    }
+  });
+});
+
+promise2.then(message => addMessage('success', message));
+
+const promise3 = new Promise((resolve, reject) => {
+  let leftButton = false;
+  let rigthButton = false;
+
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      leftButton = true;
+    }
+
+    if (e.button === 2) {
+      rigthButton = true;
+    }
+
+    if (leftButton && rigthButton) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+promise3.then(message => addMessage('success', message));
+
+function addMessage(type, text) {
+  document.body.insertAdjacentHTML('beforeend', `
+    <div class="${type}" data-qa="notification">${text}</div>
+  `);
+}
