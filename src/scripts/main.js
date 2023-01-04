fc978f7a888d79d7c601b00704dddedea58d996d
Vlad Boikov <vladboikov95@hotmail.com>
2023-01-04 12:22:43 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,74 @@
 'use strict';
+
+const promise1 = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    resolve('Second promise was resolved');
+  });
+});
+
+const promise3 = new Promise((resolve, reject) => {
+  let left = false;
+  let right = false;
+
+  document.addEventListener('click', () => {
+    left = true;
+
+    if (left === true && right === true) {
+      resolve('Third promise was resolved');
+    };
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    right = true;
+
+    if (left === true && right === true) {
+      resolve('Third promise was resolved');
+    };
+  });
+});
+
+const successHandler = data => {
+  const div = document.createElement('div');
+
+  div.className = 'success';
+  div.dataset.qa = 'notification';
+  div.textContent = data;
+  document.body.append(div);
+};
+
+const errorHandler = data => {
+  const div = document.createElement('div');
+
+  div.className = 'warning';
+  div.dataset.qa = 'notification';
+  div.textContent = data;
+  document.body.append(div);
+};
+
+promise1
+  .then(successHandler)
+  .catch(errorHandler);
+
+promise2
+  .then(successHandler)
+  .catch(errorHandler);
+
+promise3
+  .then(successHandler)
+  .catch(errorHandler);
