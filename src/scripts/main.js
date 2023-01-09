97beade3e558d9565cf877fedc71a5bff06d3f81
Oleksiy Uchaev <corob.dir@gmail.com>
2023-01-09 21:56:33 +0200

    renamed var and param for clarity :)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const addText = (number, isError) => `
-  ${number} promise was ${isError ? 'rejected' : 'resolved'}
+const addText = (promiseName, isError = false) => `
+  ${promiseName} promise was ${isError ? 'rejected' : 'resolved'}
 `;
 
 const firstPromise = new Promise((resolve, reject) => {
   document.addEventListener('mousedown', (e) => {
     e.button === 0 && resolve(addText('First'));
   });
-  setTimeout(() => reject(addText('First', 1)), 3000);
+  setTimeout(() => reject(addText('First', true)), 3000);
 });
 
 const secondPromise = new Promise((resolve) => {
@@ -20,8 +20,8 @@ const secondPromise = new Promise((resolve) => {
 const isClicked = {};
 const thirdPromise = new Promise((resolve) => {
   document.addEventListener('mousedown', (e) => {
-    e.button === 0 && (isClicked[1] = 1);
-    e.button === 2 && (isClicked[2] = 1);
+    e.button === 0 && (isClicked[1] = true);
+    e.button === 2 && (isClicked[2] = true);
     isClicked[1] && isClicked[2] && resolve(addText('Third'));
   });
 });
