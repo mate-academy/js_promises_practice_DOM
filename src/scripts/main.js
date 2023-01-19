ddf69fdceb17b4b6e6942a5938a9f146c8a10540
Vlad Fedorishchev <fedorischev23@gmail.com>
2023-01-19 18:35:33 +0300

    Fixes after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,31 +6,17 @@ bodyElement.addEventListener('contextmenu', (action) => {
   action.preventDefault();
 });
 
-// Messages classes-------------------------------------------------------------
-const messageClass = 'message';
-const secondMessageClass = 'message second-message';
-const thirdMessageClass = 'message third-message';
-const errorMessageClass = 'message error-message';
-
-// Messages types---------------------------------------------------------------
-const resolvedMessage = 'First promise was resolved';
-const rejectedMessage = 'First promise was rejected';
-const secondResolveMessage = 'Second promise was resolved';
-const thirdPromiseMessage = 'Third promise was resolved';
-
-// Show messages function-------------------------------------------------------
-function showMessage(classType, MessageType) {
+function showMessage(classType, message) {
   bodyElement.insertAdjacentHTML('beforeend', `
-        <div
-          class="${classType}"
-          data-qa="notification"
-        >
-          ${MessageType}
-        </div>
-    `);
+    <div
+      class="${classType}"
+      data-qa="notification"
+    >
+      ${message}
+    </div>
+  `);
 }
 
-// Realizing firstPromise-------------------------------------------------------
 const firstPromise = new Promise((resolve, reject) => {
   bodyElement.addEventListener('mousedown', (action) => {
     if (action.button === 0) {
@@ -42,10 +28,10 @@ const firstPromise = new Promise((resolve, reject) => {
 });
 
 firstPromise
-  .then(() => showMessage(messageClass, resolvedMessage))
-  .catch(() => showMessage(errorMessageClass, rejectedMessage));
+  .then(() => showMessage('message', 'First promise was resolved'))
+  .catch(
+    () => showMessage('message error-message', 'First promise was rejected'));
 
-// Realizing secondPromise------------------------------------------------------
 const secondPromise = new Promise((resolve) => {
   bodyElement.addEventListener('mousedown', (action) => {
     if (action.button === 0 || action.button === 2) {
@@ -54,18 +40,27 @@ const secondPromise = new Promise((resolve) => {
   });
 });
 
-secondPromise.then(() => showMessage(secondMessageClass, secondResolveMessage));
+secondPromise.then(
+  () => showMessage('message second-message', 'Second promise was resolved'));
 
-// Realizing thirdPromise-------------------------------------------------------
 const thirdPromise = new Promise((resolve) => {
-  bodyElement.addEventListener('mousedown', (actionOne) => {
-    bodyElement.addEventListener('mousedown', (actionTwo) => {
-      if ((actionOne.button === 0 && actionTwo.button === 2)
-            || (actionOne.button === 2 && actionTwo.button === 0)) {
-        resolve();
-      }
-    });
+  let leftClickCheck = false;
+  let rightClickCheck = false;
+
+  bodyElement.addEventListener('mousedown', (action) => {
+    switch (action.button) {
+      case 0:
+        leftClickCheck = true;
+        break;
+      case 2:
+        rightClickCheck = true;
+    }
+
+    if (leftClickCheck && rightClickCheck) {
+      resolve();
+    }
   });
 });
 
-thirdPromise.then(() => showMessage(thirdMessageClass, thirdPromiseMessage));
+thirdPromise.then(
+  () => showMessage('message third-message', 'Third promise was resolved'));
