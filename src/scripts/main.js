9c26b9b08591b3ded2959d95b9b3c2b243c952b7
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-01-30 12:13:26 +0200

    Finish task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,71 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('something bad happened'));
+  }, 3000);
+
+  document.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener('click', () => {
+    resolve();
+  });
+
+  document.addEventListener('contextmenu', () => {
+    resolve();
+  });
+});
+
+const thirdPromise = new Promise((resolve) => {
+  document.addEventListener('click', () => {
+    document.addEventListener('contextmenu', () => {
+      resolve();
+    });
+  });
+
+  document.addEventListener('contextmenu', () => {
+    document.addEventListener('click', () => {
+      resolve();
+    });
+  });
+});
+
+firstPromise
+  .then(() => {
+    const div = document.createElement('div');
+
+    div.classList.add('success');
+    div.dataset.qa = 'notification';
+    div.textContent = 'First promise was resolved';
+    document.querySelector('body').append(div);
+  })
+  .catch(() => {
+    const div = document.createElement('div');
+
+    div.classList.add('warning');
+    div.dataset.qa = 'notification';
+    div.textContent = 'First promise was rejected';
+    document.querySelector('body').append(div);
+  });
+
+secondPromise.then(() => {
+  const div = document.createElement('div');
+
+  div.classList.add('success');
+  div.dataset.qa = 'notification';
+  div.textContent = 'Second promise was resolved';
+  document.querySelector('body').append(div);
+});
+
+thirdPromise.then(() => {
+  const div = document.createElement('div');
+
+  div.classList.add('success');
+  div.dataset.qa = 'notification';
+  div.textContent = 'Third promise was resolved';
+  document.querySelector('body').append(div);
+});
