7324ea880d2909970f0138c5dcb6d9e9b9da4b33
Vitalii Skomskiy <skomskiy@ukr.net>
2023-02-03 14:23:32 +0200

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,63 @@
 'use strict';
+
+const firstDiv = document.createElement('div');
+const secondDiv = document.createElement('div');
+
+firstDiv.setAttribute('data-qa', 'notification');
+secondDiv.setAttribute('data-qa', 'notification');
+document.querySelector('body').appendChild(firstDiv);
+document.querySelector('body').appendChild(secondDiv);
+
+const firstPromise = new Promise(function(resolve, reject) {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved!');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected!'));
+  }, 3000);
+});
+
+const secondPromise = new Promise(function(resolve, reject) {
+  document.addEventListener('click', () => {
+    resolve('Second promise was resolved!');
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    resolve('Second promise was resolved!');
+  });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    document.addEventListener('contextmenu', () => {
+      resolve('Third promise was resolved');
+    });
+  });
+
+  document.addEventListener('contextmenu', () => {
+    document.addEventListener('click', () => {
+      resolve('Third promise was resolved');
+    });
+  });
+});
+
+firstPromise.then(resolve => {
+  firstDiv.classList.add('succes');
+  firstDiv.innerText = resolve;
+}).catch(reject => {
+  firstDiv.classList.add('warning');
+  firstDiv.textContent = reject.message;
+});
+
+secondPromise.then(resolve => {
+  secondDiv.classList.add('succes');
+  secondDiv.textContent = resolve;
+});
+
+thirdPromise.then(resolve => {
+  firstDiv.innerText = '';
+  secondDiv.classList.add('succes');
+  secondDiv.textContent = resolve;
+});
