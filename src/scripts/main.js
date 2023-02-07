d43bdb154f146b85efea5d698c23944aa182e8dc
Vladyslav Krainyk <vladkraynyk2701@gmail.com>
2023-02-07 13:08:18 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,61 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve(`First promise was resolved`);
+  });
+
+  setTimeout(() => {
+    reject(new Error(`First promise was rejected`))
+  }, 3000);
+});
+
+const leftMouseButton = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const rightMouseButton = new Promise((resolve, reject) => {
+  document.addEventListener('contextmenu', () => {
+    resolve();
+  });
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  Promise.race([leftMouseButton, rightMouseButton])
+    .then(() => {
+      resolve(`Second promise was resolved`);
+    });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  Promise.all([leftMouseButton, rightMouseButton])
+    .then(() => {
+      resolve(`Third promise was resolved`);
+    });
+});
+
+const successNotification = result => {
+  document.body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification" class="success">
+      ${result}
+    </div>
+  `);
+};
+
+const warningNotification = error => {
+  document.body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification" class="warning">
+      ${error}
+    </div>
+  `);
+};
+
+firstPromise
+  .then(successNotification)
+  .catch(warningNotification);
+
+secondPromise.then(successNotification);
+
+thirdPromise.then(successNotification);
