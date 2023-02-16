a1db421b5905219382c5898d20d1129830151cff
Kate <novak5683@gmail.com>
2023-02-16 17:06:05 +0100

    result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,67 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+new Promise((resolve, reject) => {
+  body.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+}).then(data => {
+  successHandler(data);
+}).catch(error => {
+  errorHandler(error);
+});
+
+new Promise((resolve, reject) => {
+  body.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  });
+
+  body.addEventListener('contextmenu', () => {
+    resolve('Second promise was resolved');
+  });
+}).then(data => successHandler(data));
+
+new Promise((resolve, reject) => {
+  // eslint-disable-next-line no-shadow
+  const p1 = new Promise((resolve, reject) => {
+    body.addEventListener('click', () => {
+      resolve('left click');
+    });
+  });
+
+  // eslint-disable-next-line no-shadow
+  const p2 = new Promise((resolve, reject) => {
+    body.addEventListener('contextmenu', () => {
+      resolve('right click');
+    });
+  });
+
+  Promise.all([p1, p2]).then(data => {
+    resolve('Third promise was resolved');
+  });
+}).then(data => successHandler(data));
+
+const successHandler = (message) => {
+  const div = document.createElement('div');
+
+  div.setAttribute('data-qa', 'notification');
+  div.setAttribute('class', 'success');
+  div.innerHTML = message;
+
+  body.append(div);
+};
+
+const errorHandler = (message) => {
+  const div = document.createElement('div');
+
+  div.setAttribute('data-qa', 'notification');
+  div.setAttribute('class', 'warning');
+  div.innerHTML = message;
+
+  body.append(div);
+};
