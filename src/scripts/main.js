f906a5fcbec42a51ad058c352bcf85bd1a0d11b0
Kate <novak5683@gmail.com>
2023-02-17 13:52:17 +0100

    fixed once again

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,50 +2,6 @@
 
 const body = document.querySelector('body');
 
-new Promise((resolve, reject) => {
-  body.addEventListener('click', () => {
-    resolve('First promise was resolved');
-  });
-
-  setTimeout(() => {
-    reject(new Error('First promise was rejected'));
-  }, 3000);
-}).then(data => {
-  successHandler(data);
-}).catch(error => {
-  errorHandler(error);
-});
-
-new Promise((resolve, reject) => {
-  body.addEventListener('mousedown', e => {
-    resolve('Second promise was resolved');
-  });
-}).then(data => successHandler(data));
-
-new Promise((resolve, reject) => {
-  // eslint-disable-next-line no-shadow
-  const p1 = new Promise((resolve, reject) => {
-    body.addEventListener('mousedown', (e) => {
-      if (e.which === 1) {
-        resolve('left click');
-      }
-    });
-  });
-
-  // eslint-disable-next-line no-shadow
-  const p2 = new Promise((resolve, reject) => {
-    body.addEventListener('mousedown', (e) => {
-      if (e.which === 3) {
-        resolve('right click');
-      }
-    });
-  });
-
-  Promise.all([p1, p2]).then(data => {
-    resolve('Third promise was resolved');
-  });
-}).then(data => successHandler(data));
-
 const successHandler = (message) => {
   const div = document.createElement('div');
 
@@ -65,3 +21,48 @@ const errorHandler = (message) => {
 
   body.append(div);
 };
+
+const promise1 = new Promise((resolve, reject) => {
+  body.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+promise1.then(data => {
+  successHandler(data);
+}).catch(error => {
+  errorHandler(error);
+});
+
+const promise2 = new Promise((resolve) => {
+  body.addEventListener('mousedown', e => {
+    resolve('Second promise was resolved');
+  });
+});
+
+promise2.then(data => successHandler(data));
+
+const promise3 = new Promise((resolve) => {
+  let leftBtn = false;
+  let rightBtn = false;
+
+  body.addEventListener('mousedown', (e) => {
+    if (e.which === 1) {
+      leftBtn = true;
+    }
+
+    if (e.which === 3) {
+      rightBtn = true;
+    }
+
+    if (leftBtn === true && rightBtn === true) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+promise3.then(data => successHandler(data));
