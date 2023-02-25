0aea631105ac6d0fdc83e88b6de4907757109b96
Sergei Nikolaienko <serjexdeath@gmail.com>
2023-02-25 09:26:58 +0200

    Refactored accordingly, error handling added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,12 @@
 'use strict';
+
 const firstPromise = new Promise((resolve, reject) => {
   document.addEventListener('click', () => {
     resolve('First promise was resolved');
   });
 
   setTimeout(() => {
-    reject('First promise was rejected');
+    reject(new Error('First promise was rejected'));
   }, 3000);
 });
 
@@ -17,19 +18,27 @@ const secondPromise = new Promise((resolve) => {
   });
 });
 
-const thirdPromise = new Promise ((resolve) => {
-  document.addEventListener('mousedown', (e1) => {
-    document.addEventListener('mousedown', (e2) => {
-      if ((e1.button === 0 && e2.button === 2) 
-          || (e1.button === 2 && e2.button === 0)) {
-        resolve('Third promise was resolved');
-      }
-    });
+const leftClick = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const rightClick = new Promise((resolve, reject) => {
+  document.addEventListener('contextmenu', () => {
+    resolve();
+  });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  Promise.all([leftClick, rightClick]).then(() => {
+    resolve('Third promise was resolved');
   });
 });
 
 function createDiv(className, data) {
   const divEl = document.createElement('div');
+
   divEl.classList.add(className);
   divEl.innerHTML = data;
   divEl.setAttribute('data-qa', 'notification');
@@ -37,14 +46,14 @@ function createDiv(className, data) {
   document.body.append(divEl);
 };
 
-function handleSuccess(result){
+function handleSuccess(result) {
   createDiv('success', result);
 };
 
-function handleError(error){
+function handleError(error) {
   createDiv('warning', error);
 };
 
 firstPromise.then(handleSuccess, handleError);
-secondPromise.then(handleSuccess);
-thirdPromise.then(handleSuccess);
+secondPromise.then(handleSuccess, handleError);
+thirdPromise.then(handleSuccess, handleError);
