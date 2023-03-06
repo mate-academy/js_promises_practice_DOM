495bd91eaf7cbc099b6921d9c3ab1d27b81b3e88
yana-kotsulym <yanak7170@gmail.com>
2023-03-06 20:52:23 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,91 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+  let clicked = false;
+
+  document.addEventListener('click', () => {
+    clicked = true;
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    if (!clicked) {
+      reject(new Error('First promise was rejected'));
+    }
+  }, 3000);
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    resolve('Second promise was resolved');
+  });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  let click = false;
+  let contextMenu = false;
+
+  document.addEventListener('click', () => {
+    click = true;
+
+    if (click && contextMenu) {
+      resolve('Third promise was resolved');
+    }
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    contextMenu = true;
+
+    if (click && contextMenu) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+firstPromise.then((result) => {
+  document.body.insertAdjacentHTML('afterbegin', `
+    <div data-qa="notification" class="success">
+      ${result}
+    </div>
+  `);
+}).catch((error) => {
+  document.body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification" class="warning">
+      ${error.message}
+    </div>
+  `);
+});
+
+secondPromise.then((result) => {
+  document.body.insertAdjacentHTML('afterbegin', `
+    <div data-qa="notification" class="success">
+      ${result}
+    </div>
+  `);
+}).catch((error) => {
+  document.body.insertAdjacentHTML('afterbegin', `
+    <div data-qa="notification" class="warning">
+      ${error.message}
+    </div>
+  `);
+});
+
+thirdPromise.then((result) => {
+  document.body.insertAdjacentHTML('afterbegin', `
+    <div data-qa="notification" class="success">
+      ${result}
+    </div>
+  `);
+}).catch((error) => {
+  document.body.insertAdjacentHTML('afterbegin', `
+    <div data-qa="notification" class="warning">
+      ${error.message}
+    </div>
+  `);
+});
