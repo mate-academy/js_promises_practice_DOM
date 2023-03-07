4549c617d9f35dd7a8dd063340098335165ccf2d
mykolachupyak <chupyak@gmail.com>
2023-03-07 17:22:44 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -41,19 +41,17 @@ const secondPromise = new Promise(resolve => {
 });
 
 const thirdPromise = new Promise(resolve => {
-  // eslint-disable-next-line no-shadow
+
   const leftClick = new Promise(resolve => {
     document.addEventListener('click', () => {
       resolve();
     });
   });
 
-  // eslint-disable-next-line no-shadow
-  const rightClick = new Promise(resolve => {
+  const rightClick = new Promise(resolve =>
     document.addEventListener('contextmenu', () => {
       resolve();
-    });
-  });
+    }));
 
   Promise.all([leftClick, rightClick]).then(() => {
     resolve(thirdResolveMessage);
