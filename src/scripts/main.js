3c7644f9b7beb2c06356885cca5c3c80fbb9141d
Kateryna Shylina <katya.dhilina@gmail.com>
2023-03-19 15:51:06 +0200

    solution :)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,68 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(reject, 3000);
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  });
+
+  document.addEventListener('contextmenu', () => {
+    resolve('Second promise was resolved');
+  });
+
+  if (reject) {
+
+  }
+});
+
+const thirdPromise = new Promise((resolve) => {
+  let leftClick = false;
+  let rightClick = false;
+
+  document.addEventListener('click', () => {
+    leftClick = true;
+
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+
+  document.addEventListener('contextmenu', () => {
+    rightClick = true;
+
+    if (leftClick && rightClick) {
+      resolve(`Third promise was resolved`);
+    }
+  });
+});
+
+firstPromise
+  .then(onSuccess)
+  .catch(onError);
+
+secondPromise
+  .then(onSuccess);
+
+thirdPromise
+  .then(onSuccess);
+
+function onSuccess(result) {
+  document.body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification">
+      ${result}
+    </div>
+  `);
+};
+
+function onError() {
+  document.body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification"></div>
+  `);
+};
