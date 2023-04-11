d409a4c567506ff4c967d3c99c3a3dd3b9b30f5f
Oleh Rybachuk <ribachuk202@gmail.com>
2023-04-11 17:27:02 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,61 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('something bad happened'));
+  }, 3000);
+
+  document.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener('click', () => {
+    resolve();
+  });
+
+  document.addEventListener('contextmenu', () => {
+    resolve();
+  });
+});
+
+const thirdPromise = new Promise((resolve) => {
+  document.addEventListener('click', () => {
+    document.addEventListener('contextmenu', () => {
+      resolve();
+    });
+  });
+
+  document.addEventListener('contextmenu', () => {
+    document.addEventListener('click', () => {
+      resolve();
+    });
+  });
+});
+
+firstPromise
+  .then(() => {
+    displayNotification('First promise was resolved', 'success');
+  })
+  .catch(() => {
+    displayNotification('First promise was rejected', 'warning');
+  });
+
+secondPromise.then(() => {
+  displayNotification('Second promise was resolved', 'success');
+});
+
+thirdPromise.then(() => {
+  displayNotification('Third promise was resolved', 'success');
+});
+
+function displayNotification(message, type) {
+  const div = document.createElement('div');
+
+  div.classList.add(type);
+  div.textContent = message;
+  div.dataset.qa = 'notification';
+
+  document.querySelector('body').append(div);
+}
