c11560c27fa71c538f5e8a65b5ba84770df9e04c
Maksym Avramenko <nonste@outlook.com>
2023-04-19 13:32:40 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,90 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+function success(textHTML) {
+  body.insertAdjacentHTML('beforeend', textHTML);
+}
+
+function error(textHTML) {
+  body.insertAdjacentHTML('beforeend', textHTML);
+}
+
+const firstPromise = new Promise((resolve, reject) => {
+  function handleClick(ev) {
+    ev.preventDefault();
+
+    resolve(`
+      <div div data-qa="notification">
+        First promise was resolved!
+      </div>
+    `);
+  }
+
+  document.addEventListener('click', handleClick);
+
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  function handleClick(ev) {
+    ev.preventDefault();
+
+    resolve(`
+      <div div data-qa="notification">
+        Second promise was resolved!
+      </div>
+    `);
+  }
+
+  document.addEventListener('click', handleClick);
+  document.addEventListener('contextmenu', handleClick);
+});
+
+const thirdPromice = new Promise((resolve, reject) => {
+  let click;
+  let contextmenu;
+
+  const textHTML = `
+      <div div data-qa="notification">
+        Third promise was resolved!
+      </div>
+    `;
+
+  document.addEventListener('click', () => {
+    click = true;
+
+    if (click && contextmenu) {
+      resolve(textHTML);
+    }
+  });
+
+  document.addEventListener('contextmenu', () => {
+    contextmenu = true;
+
+    if (click && contextmenu) {
+      resolve(textHTML);
+    }
+  });
+});
+
+firstPromise
+  .then(result => {
+    success(result);
+  }).catch(() => {
+    error(`
+    <div div data-qa="notification">
+      First promise was rejected!
+    </div>
+  `);
+  });
+
+secondPromise.then(result => {
+  success(result);
+});
+
+thirdPromice.then(result => {
+  success(result);
+});
