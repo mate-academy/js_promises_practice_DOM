7dd717b48e87105194e6a8c78cfce471cf176e98
Ilia <koleshnik160799@gmail.com>
2023-04-23 12:24:51 +0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,68 @@
 'use strict';
+const body = document.querySelector('body');
+
+const promise1 = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  })
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+})
+
+const promise2 = new Promise((resolve) => {
+  document.addEventListener('mousedown', (e) => {
+    if(e.buttons === 1 || e.buttons === 2) {
+      resolve('Second promise was resolved');
+    }
+  })
+})
+
+const promise3 = new Promise((resolve, reject) => {
+  let leftClick = false;
+  let rightClick = false;
+  document.addEventListener('mousedown', (e) => {
+    
+    if(e.buttons === 1) {
+      leftClick = true;
+    } 
+
+    if(e.buttons === 2) {
+      rightClick = true;
+    }
+
+    if(rightClick && leftClick) {
+      resolve('Third promise was resolved');
+    }
+  })
+})
+
+
+promise1.then(result => {
+  body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification" class="success">${result}</div>
+  `)
+})
+.catch(result => {
+  body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification" class="warning ">${result}</div>
+  `)
+})
+
+promise2.then(result => {
+  body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification" class="success">${result}</div>
+  `)
+})
+
+
+promise3.then(result => {
+  body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification" class="success">${result}</div>
+  `)
+})
+
+
+
+
