bf077ee6ab3f7fb49e947e66cf69b1414284dd99
Nadiein Oleh <olegnadein1994@gmail.com>
2023-04-28 17:15:51 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,49 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.body.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => reject(new Error('First promise was rejected')), 3000);
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  document.body.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    resolve('Second promise was resolved');
+  });
+
+  document.body.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  resolve('Third promise was resolved');
+});
+
+secondPromise
+  .then((result) => {
+    createMessage('success', result);
+
+    return firstPromise;
+  })
+  .then((result) => {
+    createMessage('success', result);
+
+    return thirdPromise;
+  }).then(result => {
+    createMessage('success', result);
+  })
+  .catch(err => {
+    createMessage('warning', err);
+  });
+
+const createMessage = (className, textContent) => {
+  document.body.insertAdjacentHTML('beforebegin', `
+  <div class="${className}" qa="notification">
+    ${textContent}
+  </div>
+  `);
+};
