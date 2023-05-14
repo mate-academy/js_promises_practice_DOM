9830faa869a04760046b7c903492900b0a79e244
Ihor Zakharov <ihor.o.zakharov@gmail.com>
2023-05-14 23:27:47 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,77 @@
-'use strict';
+"use strict";
+
+document.addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+})
+
+function firstPromiseResolver(resolve, reject) {
+  document.addEventListener("click", () => {
+    resolve();
+  });
+
+  setTimeout(() => {
+    reject();
+  }, 3000);
+}
+
+function secondPromiseResolver(resolve, reject) {
+  document.addEventListener("click", () => {
+    resolve();
+  });
+
+  document.addEventListener("contextmenu", () => {
+    resolve();
+  });
+}
+
+function thirdPromiseResolver(resolve, reject) {
+  let leftClick = false;
+  let rightClick = false;
+
+  function checkClicks() {
+    if (leftClick && rightClick) {
+      resolve();
+    }
+  }
+
+  document.addEventListener("click", () => {
+    leftClick = true;
+    checkClicks();
+  });
+
+  document.addEventListener("contextmenu", () => {
+    rightClick = true;
+    checkClicks();
+  });
+};
+
+const createNotification = (success, promiseNumber) => {
+  const notification = document.createElement("div");
+  notification.setAttribute("data-qa", "notification");
+
+  if (success) {
+    notification.className = "success";
+    notification.innerText = `${promiseNumber} promise was resolved`;
+  } else {
+    notification.className = "warning";
+    notification.innerText = `${promiseNumber} promise was rejected`;
+  }
+
+  document.body.append(notification);
+};
+
+const firstPromise = new Promise(firstPromiseResolver);
+const secondPromise = new Promise(secondPromiseResolver);
+const thirdPromise = new Promise(thirdPromiseResolver);
+
+firstPromise
+  .then(() => createNotification(true, "First"))
+  .catch(() => createNotification(false, "First"));
+
+secondPromise
+  .then(() => createNotification(true, "Second"))
+  .catch(() => createNotification(false, "Second"));
+
+thirdPromise
+  .then(() => createNotification(true, "Third"))
+  .catch(() => createNotification(false, "Third"));
