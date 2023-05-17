826c05389b553d73e9ad2adca221101e6541685e
Veronika Pastushkova <d9169393507@gmai.com>
2023-05-17 17:15:17 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,79 @@
 'use strict';
+
+const body = document.querySelector('body');
+const message = document.createElement('div');
+
+const firstPromise = new Promise((resolve, reject) => {
+  body.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+firstPromise
+  .then(result => {
+    message.setAttribute('data-qa', 'notification');
+    message.className = 'success';
+    message.innerText = result;
+    body.append(message);
+  })
+  .catch(error => {
+    message.setAttribute('data-qa', 'notification');
+    message.className = 'warning';
+    message.innerText = error;
+    body.append(message);
+  });
+
+const secondPromise = new Promise((resolve) => {
+  body.addEventListener('mousedown', (e) => {
+    if (e.button === 0 || e.button === 2) {
+      resolve('Second promise was resolved');
+    }
+  });
+});
+
+secondPromise
+  .then(result => {
+    message.setAttribute('data-qa', 'notification');
+    message.className = 'success';
+    message.innerText = result;
+    body.append(message);
+  })
+  .catch(null);
+
+const thirdPromise = new Promise((resolve) => {
+  let leftBtn = false;
+  let rightBtn = false;
+
+  body.addEventListener('click', () => {
+    leftBtn = true;
+
+    if (leftBtn && rightBtn) {
+      resolve('Third promise was resolved');
+    }
+    /* if (e.button === 0 && e.button === 2) {
+      leftBtn = true;
+      rightBtn = true;
+      resolve('Third promise was resolved');
+    } */
+  });
+
+  body.addEventListener('contextmenu', () => {
+    rightBtn = true;
+
+    if (leftBtn && rightBtn) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+thirdPromise
+  .then(result => {
+    message.setAttribute('data-qa', 'notification');
+    message.className = 'success';
+    message.innerText = result;
+    body.append(message);
+  });
