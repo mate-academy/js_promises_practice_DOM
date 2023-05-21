9ec019ffe41e1199140bff1ad5d0307ce646258e
Absolution26 <zet2571428@gmail.com>
2023-05-21 15:11:31 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,71 @@
 'use strict';
+
+// Add `success` and `error` handlers to each promise
+// to show `<div data-qa="notification">` with `success`
+// or `warning` class and a promise message.
+
+function createBlockSuccess(text) {
+  const block = document.createElement('div');
+
+  block.setAttribute('data-qa', 'notification');
+  block.innerText = text;
+  block.className = 'success';
+  document.body.append(block);
+}
+
+function createBlockError(text) {
+  const block = document.createElement('div');
+
+  block.setAttribute('data-qa', 'notification');
+  block.innerText = text;
+  block.className = 'warning';
+  document.body.append(block);
+}
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.body.addEventListener('click', () => {
+    resolve(`First promise was resolved`);
+  });
+
+  setTimeout(() => {
+    reject(new Error(`First promise was rejected`));
+  }, 3000);
+});
+
+firstPromise.then((str) => createBlockSuccess(str));
+firstPromise.catch((str) => createBlockError(str));
+
+const secondPromise = new Promise((resolve, reject) => {
+  document.body.addEventListener('contextmenu', () => {
+    resolve(`Second promise was resolved`);
+  });
+
+  document.body.addEventListener('click', () => {
+    resolve(`Second promise was resolved`);
+  });
+});
+
+secondPromise.then((str) => createBlockSuccess(str));
+
+const thirdPromise = new Promise((resolve, reject) => {
+  let leftClick = false;
+  let rightClick = false;
+
+  document.body.addEventListener('click', () => {
+    if (leftClick === true) {
+      resolve(`Third promise was resolved`);
+    }
+
+    rightClick = true;
+  });
+
+  document.body.addEventListener('contextmenu', () => {
+    if (rightClick === true) {
+      resolve(`Third promise was resolved`);
+    }
+
+    leftClick = true;
+  });
+});
+
+thirdPromise.then((str) => createBlockSuccess(str));
