c5520cd629256ce2455ee25a9b546bb949ad85c5
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-06-01 00:49:33 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,49 +7,38 @@ function apenndEl(response) {
 
   div.setAttribute('data-qa', 'notification');
 
-  if (response === 'resolve') {
-    div.className = 'success';
-  } else {
-    div.className = 'error';
-  }
+  div.className = response === 'resolve' ? 'success' : 'error';
   div.textContent = response;
   body.append(div);
 }
 
-let rightClick = false;
-let leftClick = false;
-
-const firstPromise = new Promise((resolve, reject) => {
-  body.addEventListener('mousedown', (e) => {
-    if (e.button === 0) {
-      leftClick = true;
-      resolve('First promise was resolved');
-    }
+const clickPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
   });
 
   setTimeout(() => {
-    if (!leftClick) {
-      return reject(new Error('First promise was rejected'));
-    }
+    return reject(new Error('First promise was rejected'));
   }, 3000);
 });
 
-const secondPromise = new Promise((resolve, reject) => {
-  body.addEventListener('mousedown', (e) => {
-    e.button === 2 ? rightClick = true : leftClick = true;
-    resolve('Second promise was resolved');
+const contextMenuPromise = new Promise(resolve => {
+  document.addEventListener('contextmenu', () => {
+    resolve();
   });
+});
+
+const secondPromise = new Promise(resolve => {
+  Promise.any([clickPromise, contextMenuPromise])
+    .then(value => resolve('Second promise was resolved'));
 });
 
 const thirdPromise = new Promise((resolve, reject) => {
-  body.addEventListener('mousedown', (e) => {
-    if (rightClick && leftClick) {
-      resolve('Third promise was resolved');
-    }
-  });
+  Promise.all([clickPromise, contextMenuPromise])
+    .then(value => resolve('Third promise was resolved'));
 });
 
-firstPromise.then((resolve) => apenndEl(resolve))
+clickPromise.then((resolve) => apenndEl(resolve))
   .catch((reject) => apenndEl(reject));
 
 secondPromise.then((resolve) => apenndEl(resolve));
