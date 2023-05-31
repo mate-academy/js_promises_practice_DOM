1609512e0f4a3aea6bd9342f78e5d3c84996626f
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-05-31 17:33:53 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,57 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+function apenndEl(response) {
+  const div = document.createElement('div');
+
+  div.setAttribute('data-qa', 'notification');
+
+  if (response === 'resolve') {
+    div.className = 'success';
+  } else {
+    div.className = 'error';
+  }
+  div.textContent = response;
+  body.append(div);
+}
+
+let rightClick = false;
+let leftClick = false;
+
+const firstPromise = new Promise((resolve, reject) => {
+  body.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      leftClick = true;
+      resolve('First promise was resolved');
+    }
+  });
+
+  setTimeout(() => {
+    if (!leftClick) {
+      return reject(new Error('First promise was rejected'));
+    }
+  }, 3000);
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  body.addEventListener('mousedown', (e) => {
+    e.button === 2 ? rightClick = true : leftClick = true;
+    resolve('Second promise was resolved');
+  });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  body.addEventListener('mousedown', (e) => {
+    if (rightClick && leftClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+firstPromise.then((resolve) => apenndEl(resolve))
+  .catch((reject) => apenndEl(reject));
+
+secondPromise.then((resolve) => apenndEl(resolve));
+
+thirdPromise.then((resolve) => apenndEl(resolve));
