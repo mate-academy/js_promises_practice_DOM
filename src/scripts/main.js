e2ffd65c6f05cfe1464533fc9b1b9b4566739242
Vlad Danilenko <gabiolario@gmail.com>
2023-06-01 07:22:49 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,63 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+const notification = (message, className) => {
+  body.insertAdjacentHTML('beforeend', `
+    <div data-qa="notification" class="${className}">${message}</div>`);
+};
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      resolve(`First promise was resolved`);
+    }
+  });
+
+  setTimeout(() => reject(new Error(`First promise was rejected`)), 3000);
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0 || e.button === 2) {
+      resolve(`Second promise was resolved`);
+    }
+  });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  let leftButton = false;
+  let rightButton = false;
+
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      leftButton = true;
+    }
+
+    if (e.button === 2) {
+      rightButton = true;
+    }
+
+    if (leftButton && rightButton) {
+      resolve(`Third promise was resolved`);
+    }
+  });
+});
+
+firstPromise
+  .then((result) => {
+    notification(result, 'success');
+  })
+  .catch((error) => {
+    notification(error.message, 'warning');
+  });
+
+secondPromise
+  .then((result) => {
+    notification(result, 'success');
+  });
+
+thirdPromise
+  .then((result) => {
+    notification(result, 'success');
+  });
