63df7971566067a14a2e57523b17e4fa6c90cc66
Valeria Oleynik <oleynik.valeria27@gmail.com>
2023-06-06 17:51:49 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,59 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+const showMessage = (text, className) => {
+  const div = document.createElement('div');
+
+  div.dataset.qa = 'notification';
+  div.classList.add(className);
+  div.textContent = text;
+  body.append(div);
+};
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      resolve('First promise was resolved');
+    }
+  });
+
+  setTimeout(() => reject(new Error('First promise was rejected')), 3000);
+});
+
+firstPromise
+  .then(success => showMessage(success, 'success'))
+  .catch(error => showMessage(error, 'warning'));
+
+const secondPromise = new Promise((resolve, reject) => {
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0 || e.button === 2) {
+      resolve('Second promise was resolved');
+    }
+  });
+});
+
+secondPromise
+  .then(success => showMessage(success, 'success'));
+
+const thirdPromise = new Promise((resolve, reject) => {
+  let leftClick = false;
+  let rightClick = false;
+
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      leftClick = true;
+    }
+
+    if (e.button === 2) {
+      rightClick = true;
+    }
+
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+thirdPromise
+  .then(success => showMessage(success, 'success'));
