d3bba2fb40902701a569bd5b03d832f74e524439
OlegRak86 <rakolegit@gmail.com>
2023-06-08 16:38:53 +0300

    js_promises_practice_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,55 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+const logo = body.querySelector('.logo');
+
+function createPromise(masegeClass, numberPromise, mouseEvent) {
+  const resolver = (complite, cancel) => {
+    logo.addEventListener(`${mouseEvent}`, (e) => {
+      e.preventDefault();
+
+      complite(
+        `<div class=${masegeClass}
+          data-qa="notification"
+          >
+            ${numberPromise} promise was resolved
+          </div>`
+      );
+    });
+
+    setTimeout(() => {
+      cancel(`<div class="error"
+    data-qa="notification"
+    >
+    First promise was rejected
+    </div>`);
+    }, 3000);
+  };
+
+  return new Promise(resolver);
+}
+
+const promis1 = createPromise('success', 'First', 'click');
+
+promis1.then(suc => {
+  body.insertAdjacentHTML('beforeend', suc);
+
+  const promis2 = createPromise('success', 'Second', 'click');
+
+  return promis2;
+}).catch(err => {
+  body.insertAdjacentHTML('beforeend', err);
+
+  const promis2 = createPromise('success', 'Second', 'click');
+
+  return promis2;
+}).then(suc => {
+  body.insertAdjacentHTML('beforeend', suc);
+
+  const promis3 = createPromise('success', 'Third', 'contextmenu');
+
+  return promis3;
+}).then(suc => {
+  body.insertAdjacentHTML('beforeend', suc);
+});
