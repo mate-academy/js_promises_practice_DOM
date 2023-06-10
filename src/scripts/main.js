35bd7d3f9ca774f488d23783d393588211e4aaab
OlegRak86 <rakolegit@gmail.com>
2023-06-10 19:28:35 +0300

    js_promises_practice_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -32,22 +32,38 @@ function createPromise(messageClass, numberPromise, mouseEvent = 'click') {
 
 const promis1 = createPromise('success', 'First');
 
+function createPromise2(messageClass, numberPromise, mouseEvent = 'click') {
+  return new Promise(function resolver(resolve) {
+    logo.addEventListener(`${mouseEvent}`, (e) => {
+      e.preventDefault();
+
+      resolve(
+        `<div class='${messageClass}'
+            data-qa="notification"
+            >
+              ${numberPromise} promise was resolved
+            </div>`
+      );
+    });
+  });
+};
+
 promis1.then(suc => {
   body.insertAdjacentHTML('beforeend', suc);
 
-  const promis2 = createPromise('success', 'Second');
+  const promis2 = createPromise2('success', 'Second');
 
   return promis2;
 }).catch(err => {
   body.insertAdjacentHTML('beforeend', err);
 
-  const promis2 = createPromise('success', 'Second');
+  const promis2 = createPromise2('success', 'Second');
 
   return promis2;
 }).then(suc => {
   body.insertAdjacentHTML('beforeend', suc);
 
-  const promis3 = createPromise('success', 'Third', 'contextmenu');
+  const promis3 = createPromise2('success', 'Third', 'contextmenu');
 
   return promis3;
 }).then(suc => {
