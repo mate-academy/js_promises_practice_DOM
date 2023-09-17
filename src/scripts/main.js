68389b2a974aca531292fa9755fe802ba9811eb5
Eugene Kulish <genekulish13@gmail.com>
2023-09-17 10:17:57 +0300

    solve the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,77 @@
 'use strict';
+
+function appendNotification(number, result) {
+  const notification = document.createElement('div');
+
+  notification.dataset.qa = 'notification';
+
+  notification.className = `notification ${
+    (result === 'success') ? 'success' : 'warning'
+  }`;
+
+  notification.classList.add(number.toLowerCase());
+
+  notification.innerText = `${number} promise was ${
+    (result === 'success') ? 'resolved' : 'rejected'
+  }`;
+
+  document.body.append(notification);
+}
+
+const firstPromise = new Promise((resolve, reject) => {
+  const promiseNumber = 'First';
+
+  document.addEventListener('click', () => {
+    resolve(promiseNumber);
+  });
+
+  setTimeout(() => reject(promiseNumber), 3000);
+});
+
+const secondPromise = Promise.race([
+  new Promise(resolve => {
+    const promiseNumber = 'Second';
+
+    document.addEventListener('click', () => {
+      resolve(promiseNumber);
+    });
+  }),
+
+  new Promise(resolve => {
+    const promiseNumber = 'Second';
+
+    document.addEventListener('contextmenu', () => {
+      resolve(promiseNumber);
+    });
+  }),
+]);
+
+const thirdPromise = Promise.all([
+  new Promise(resolve => {
+    const promiseNumber = 'Third';
+
+    document.addEventListener('click', () => {
+      resolve(promiseNumber);
+    });
+  }),
+
+  new Promise(resolve => {
+    const promiseNumber = 'Third';
+
+    document.addEventListener('contextmenu', () => {
+      resolve(promiseNumber);
+    });
+  }),
+]);
+
+firstPromise
+  .then(promiseNumber => appendNotification(promiseNumber, 'success'))
+  .catch(promiseNumber => appendNotification(promiseNumber, 'error'));
+
+secondPromise
+  .then(promiseNumber => appendNotification(promiseNumber, 'success'))
+  .catch(promiseNumber => appendNotification(promiseNumber, 'error'));
+
+thirdPromise
+  .then(promiseNumber => appendNotification(promiseNumber[0], 'success'))
+  .catch(promiseNumber => appendNotification(promiseNumber[0], 'error'));
