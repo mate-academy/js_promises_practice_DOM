aa41be9bfe89abb4e2b64d85525a1e036996157e
Dmytro Lazarenko <lazarenkodmytro@gmail.com>
2023-09-27 22:23:47 +0200

    js_promises_practice_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,49 @@
 'use strict';
+
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const error = new Error('Promise rejected after 3 seconds');
+
+    reject(error);
+  }, 3000);
+});
+
+promise1
+  .then(() => {
+    const messageDiv = document.createElement('div');
+
+    messageDiv.className = 'message';
+    messageDiv.textContent = 'Promise was resolved!';
+    document.body.appendChild(messageDiv);
+  })
+  .catch(() => {
+    const errorMessageDiv = document.createElement('div');
+
+    errorMessageDiv.className = 'message error-message';
+    errorMessageDiv.textContent = 'Promise was rejected!';
+    document.body.appendChild(errorMessageDiv);
+  });
+
+promise2
+  .then(() => {
+    const messageDiv = document.createElement('div');
+
+    messageDiv.className = 'message';
+    messageDiv.textContent = 'Promise was resolved!';
+    document.body.appendChild(messageDiv);
+  })
+  .catch(() => {
+    const errorMessageDiv = document.createElement('div');
+
+    errorMessageDiv.className = 'message error-message';
+    errorMessageDiv.textContent = 'Promise was rejected!';
+    document.body.appendChild(errorMessageDiv);
+  });
