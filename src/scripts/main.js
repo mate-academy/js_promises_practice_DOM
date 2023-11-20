546f8055dffac0c001d7441b6787127396b47094
Mirosław Wandyk <barosz1111@gmail.com>
2023-11-20 16:41:30 +0100

    early acces - it works

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,24 @@
 
 const body = document.querySelector('body');
 
+document.oncontextmenu = rightClick;
+
+let right = false;
+let left = false;
+
+// there HAS to be bettter solution for right click. I just didnt find it yet
+
+function rightClick(clickEvent) {
+  clickEvent.preventDefault();
+  right = true;
+  // return false;
+}
+
 const firstPromise = new Promise((resolve, reject) => {
   document.addEventListener('click', () => {
-    resolve();
+    if (event.button === 0) {
+      resolve();
+    }
   });
   setTimeout(() => reject(new Error()), 3000);
 });
@@ -25,9 +40,56 @@ const addBadMessage = () => {
   body.appendChild(succesDiv);
 };
 
+// not sure aobut one below. Should i override usual rightclick utilities?
+
+const secondPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    if (event.button === 0 || event.button === 2) {
+      resolve();
+    }
+  });
+});
+
+const addSecondMessage = () => {
+  const succesDiv = document.createElement('div');
+
+  succesDiv.className = 'message__second';
+  succesDiv.innerText = 'Second promise was resolved!';
+  body.appendChild(succesDiv);
+};
+
+const thirdPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    if (event.button === 0) {
+      left = true;
+    }
+
+    if (event.button === 2) {
+      right = true;
+    }
+
+    if (left === true && right === true) {
+      resolve();
+    }
+  });
+});
+
+const addThirdMessage = () => {
+  const succesDiv = document.createElement('div');
+
+  succesDiv.className = 'message__third';
+  succesDiv.innerText = 'Third promise was resolved!';
+  body.appendChild(succesDiv);
+};
 // not sure if it should be div, or pop up message
 // it's div for now
 
 firstPromise
   .then(addMessage)
   .catch(addBadMessage);
+
+secondPromise
+  .then(addSecondMessage);
+
+thirdPromise
+  .then(addThirdMessage);
