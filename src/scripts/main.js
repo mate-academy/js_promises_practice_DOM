bbe07719f318e79ccd76b07710c71f96875189f4
Mirosław Wandyk <barosz1111@gmail.com>
2023-11-20 15:58:15 +0100

    first promise done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve();
+  });
+  setTimeout(() => reject(new Error()), 3000);
+});
+
+const addMessage = () => {
+  const succesDiv = document.createElement('div');
+
+  succesDiv.className = 'message';
+  succesDiv.innerText = 'First promise was resolved!';
+  body.appendChild(succesDiv);
+};
+
+const addBadMessage = () => {
+  const succesDiv = document.createElement('div');
+
+  succesDiv.className = 'message error-message';
+  succesDiv.innerText = 'First promise was rejected';
+  body.appendChild(succesDiv);
+};
+
+// not sure if it should be div, or pop up message
+// it's div for now
+
+firstPromise
+  .then(addMessage)
+  .catch(addBadMessage);
