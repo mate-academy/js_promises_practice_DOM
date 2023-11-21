7e188646956b53ad6961ef2f634ad0d1ab830ebc
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2023-11-21 11:14:10 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,93 @@
 'use strict';
+
+const logo = document.querySelector('.logo');
+
+const firstPromise = () => {
+  return new Promise((resolve, reject) => {
+    logo.addEventListener('click', () => {
+      resolve('First promise was resolved');
+    });
+
+    setTimeout(() => {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject('First promise was rejected');
+    }, 3000);
+  });
+};
+
+firstPromise()
+  .then(message => {
+    appendMessage(message);
+  })
+  .catch(value => {
+    appendErrorMessage(value);
+  });
+
+const secondPromise = () => {
+  return new Promise((resolve) => {
+    secondClick(resolve);
+  });
+};
+
+secondPromise()
+  .then(message => {
+    appendMessage(message);
+  });
+
+function secondClick(resolve) {
+  logo.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  });
+
+  logo.addEventListener('contextmenu', () => {
+    resolve('Second promise was resolved');
+  });
+}
+
+const thirdPromise = () => {
+  return new Promise((resolve) => {
+    thirdClick(resolve);
+  });
+};
+
+thirdPromise()
+  .then(message => {
+    appendMessage(message);
+  });
+
+function thirdClick(resolve) {
+  let leftClick = false;
+  let rightClick = false;
+
+  logo.addEventListener('click', () => {
+    leftClick = true;
+    checkAndResolve();
+  });
+
+  logo.addEventListener('contextmenu', () => {
+    rightClick = true;
+    checkAndResolve();
+  });
+
+  function checkAndResolve() {
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  }
+}
+
+function appendMessage(message) {
+  const messageDiv = document.createElement('div');
+
+  messageDiv.className = 'message';
+  messageDiv.textContent = message;
+  document.body.appendChild(messageDiv);
+}
+
+function appendErrorMessage(error) {
+  const errorMessageDiv = document.createElement('div');
+
+  errorMessageDiv.className = 'message error-message';
+  errorMessageDiv.textContent = error;
+  document.body.appendChild(errorMessageDiv);
+}
