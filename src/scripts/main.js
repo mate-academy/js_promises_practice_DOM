d4a599efcb1e7003e69ce9bf675256753c27794a
UlianaChorna <110450537+UlianaChorna@users.noreply.github.com>
2023-12-19 13:09:02 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,70 @@
 'use strict';
+
+const success = (result) => {
+  const created = document.createElement('div');
+
+  created.className = 'success';
+  created.dataset.qa = 'notification';
+  created.innerText = result;
+  document.body.appendChild(created);
+};
+
+const error = (result) => {
+  const created = document.createElement('div');
+
+  created.className = 'warning';
+  created.dataset.qa = 'notification';
+  created.innerText = result;
+  document.body.appendChild(created);
+};
+
+const promise1 = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  });
+
+  document.addEventListener('contextmenu', () => {
+    resolve('Second promise was resolved');
+  });
+});
+
+promise1
+  .then(message => success(message))
+  .catch(error);
+
+promise2
+  .then(message => success(message));
+
+const promise3 = new Promise((resolve) => {
+  let left = false;
+  let right = false;
+
+  document.addEventListener('click', () => {
+    left = true;
+
+    if (right) {
+      resolve('Third promise was resolved');
+    }
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    right = true;
+
+    if (left) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+promise3.then(success);
