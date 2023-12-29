f0f135d80cc00267e97a9bf401281833d4f34f80
Robert Szałapak <robsza1212@gmail.com>
2023-12-29 17:36:28 +0100

    solution_of_task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,48 @@
 'use strict';
+
+const taskBlock = document.createElement('div');
+
+taskBlock.setAttribute('data-qa = "notification"');
+document.body.appendChild(taskBlock);
+
+const firstPromise = new Promise(function(resolve, reject) {
+  taskBlock.addEventListener('click', () => {
+    resolve('success');
+  });
+
+  setTimeout(() => {
+    reject(new Error('error'));
+  }, 3000);
+});
+
+const secondPromise = new Promise(function(resolve, reject) {
+  taskBlock.addEventListener('left' || 'right', () => {
+    resolve('success');
+  });
+});
+
+const thirdPromise = new Promise(function(resolve, reject) {
+  taskBlock.addEventListener('left' && 'right', () => {
+    resolve('success');
+  });
+});
+
+firstPromise.then(() => {
+  document.body.insertAdjacentHTML('beforeend',
+    '<div>First promise was resolved</div>');
+});
+
+firstPromise.catch(() => {
+  document.body.insertAdjacentHTML('beforeend',
+    '<div>First promise was rejected</div>');
+});
+
+secondPromise.then(() => {
+  document.body.insertAdjacentHTML('beforeend',
+    '<div>Second promise was resolved</div>');
+});
+
+thirdPromise.then(() => {
+  document.body.insertAdjacentHTML('beforeend',
+    '<div>Third promise was resolved</div>');
+});
