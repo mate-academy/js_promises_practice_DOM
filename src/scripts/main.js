04b8adb2699185c39bcb23929b7e38c0990c1a40
Pawel Nowicki <pawel.nowicki87@tlen.pl>
2024-01-19 23:39:25 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,86 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  function updateNotification(stat, message) {
+    const notificationDiv = document.createElement('div');
+
+    notificationDiv.setAttribute('data-qa', 'notification');
+    notificationDiv.innerHTML = message;
+    notificationDiv.classList.add(stat);
+    document.body.appendChild(notificationDiv);
+  }
+
+  const firstPromise = new Promise((resolve, reject) => {
+    function handleClick() {
+      resolve('First promise was resolved');
+      document.removeEventListener('click', handleClick);
+    }
+
+    document.addEventListener('click', handleClick);
+
+    const timeoutId = setTimeout(() => {
+      clearTimeout(timeoutId);
+      reject(new Error('First promise was rejected'));
+      document.removeEventListener('click', handleClick);
+    }, 3000);
+  });
+
+  firstPromise
+    .then((message) => {
+      updateNotification('success', message);
+    })
+    .catch((error) => {
+      updateNotification('warning', error.message);
+    });
+
+  const secondPromise = new Promise((resolve) => {
+    function handleClick(events) {
+      if (events.type === 'click' && (events.button === 0
+        || events.button === 2)) {
+        resolve('Second promise was resolved');
+        document.removeEventListener('click', handleClick);
+      }
+    }
+
+    document.addEventListener('click', handleClick);
+  });
+
+  secondPromise.then((message) => {
+    updateNotification('success', message);
+  });
+
+  const thirdPromise = new Promise((resolve) => {
+    let leftClick = false;
+    let rightClick = false;
+
+    function handleLeftClick() {
+      leftClick = true;
+      checkBothClicks();
+    }
+
+    function handleRightClick(events) {
+      if (events.type === 'contextmenu') {
+        events.preventDefault();
+        rightClick = true;
+        checkBothClicks();
+      }
+    }
+
+    function checkBothClicks() {
+      if (leftClick && rightClick) {
+        resolve('Both left and right clicks happened');
+        document.removeEventListener('click', handleLeftClick);
+        document.removeEventListener('contextmenu', handleRightClick);
+      }
+    }
+
+    document.addEventListener('click', handleLeftClick);
+    document.addEventListener('contextmenu', handleRightClick);
+  });
+
+  thirdPromise.then((message) => {
+    updateNotification('success', message);
+  }).catch((error) => {
+    updateNotification('warning', error.message);
+  });
+});
