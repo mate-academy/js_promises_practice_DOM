eaaa0ee22f2aa50158750619f11a7ae5d49400a8
Pawel Nowicki <pawel.nowicki87@tlen.pl>
2024-03-23 11:44:28 +0100

    refactored code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,85 +1,87 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function() {
-  function updateNotification(stat, message) {
-    const notificationDiv = document.createElement('div');
+document.addEventListener('contextmenu', (rc) => {
+  rc.preventDefault();
+});
 
-    notificationDiv.setAttribute('data-qa', 'notification');
-    notificationDiv.innerHTML = message;
-    notificationDiv.classList.add(stat);
-    document.body.appendChild(notificationDiv);
-  }
+const firstPromise = new Promise((resolve, reject) => {
+  const clicked = document.documentElement;
 
-  const firstPromise = new Promise((resolve, reject) => {
-    function handleClick() {
-      resolve('First promise was resolved');
-      document.removeEventListener('click', handleClick);
-    }
-
-    document.addEventListener('click', handleClick);
-
-    const timeoutId = setTimeout(() => {
-      clearTimeout(timeoutId);
-      reject(new Error('First promise was rejected'));
-      document.removeEventListener('click', handleClick);
-    }, 3000);
+  clicked.addEventListener('click', () => {
+    resolve('First promise was resolved');
   });
 
-  firstPromise
-    .then((message) => {
-      updateNotification('success', message);
-    })
-    .catch((error) => {
-      updateNotification('warning', error.message);
-    });
+  setTimeout(() => {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    reject('First promise was rejected');
+  }, 3000);
+});
 
-  const secondPromise = new Promise((resolve) => {
-    function handleClick(events) {
-      if (events.type === 'click' && (events.button === 0
-        || events.button === 2)) {
-        resolve('Second promise was resolved');
-        document.removeEventListener('click', handleClick);
-      }
-    }
+const notification = document.createElement('div');
 
-    document.addEventListener('click', handleClick);
+notification.setAttribute('data-qa', 'notification');
+notification.style.padding = '15px';
+notification.style.background = 'yellow';
+
+firstPromise.then(result => {
+  notification.innerHTML = result;
+  document.body.appendChild(notification);
+})
+  .catch(reject => {
+    notification.innerHTML = reject;
+    document.body.appendChild(notification);
   });
 
-  secondPromise.then((message) => {
-    updateNotification('success', message);
-  });
-
-  const thirdPromise = new Promise((resolve) => {
-    let leftClickElement = null;
-    let rightClickElement = null;
-
-    function handleLeftClick(cli) {
-      leftClickElement = cli.target;
-      checkBothClicks();
+const secondPromise = new Promise(resolve => {
+  document.addEventListener('mousedown', e => {
+    if (e.button === 0 || e.button === 2) {
+      resolve('Second promise was resolved');
     }
-
-    function handleRightClick(cli) {
-      cli.preventDefault();
-      rightClickElement = cli.target;
-      checkBothClicks();
-    }
-
-    function checkBothClicks() {
-      if (leftClickElement && rightClickElement
-        && leftClickElement !== rightClickElement) {
-        resolve('Third promise was resolved');
-        document.removeEventListener('click', handleLeftClick);
-        document.removeEventListener('contextmenu', handleRightClick);
-      }
-    }
-
-    document.addEventListener('click', handleLeftClick);
-    document.addEventListener('contextmenu', handleRightClick);
-  });
-
-  thirdPromise.then((message) => {
-    updateNotification('success', message);
-  }).catch((error) => {
-    updateNotification('warning', error.message);
   });
 });
+
+const secondNotification = document.createElement('div');
+
+secondNotification.setAttribute('data-qa', 'notification');
+secondNotification.style.padding = '15px';
+secondNotification.style.background = 'green';
+
+secondPromise.then(result => {
+  secondNotification.innerHTML = result;
+  document.body.appendChild(secondNotification);
+});
+
+const thirdPromise = new Promise(resolve => {
+  const buttonPressed = {
+    left: false,
+    right: false,
+  };
+
+  const checkButtons = () => {
+    if (buttonPressed.left === true && buttonPressed.right === true) {
+      resolve('Third promise was resolved');
+    }
+  };
+
+  document.addEventListener('mousedown', e => {
+    if (e.button === 0) {
+      buttonPressed.left = true;
+      checkButtons();
+    } else if (e.button === 2) {
+      buttonPressed.right = true;
+      checkButtons();
+    }
+  });
+});
+
+const thirdNotification = document.createElement('div');
+
+thirdNotification.setAttribute('data-qa', 'notification');
+thirdNotification.style.padding = '15px';
+thirdNotification.style.background = 'red';
+
+thirdPromise.then(result => {
+  thirdNotification.innerHTML = result;
+  document.body.appendChild(thirdNotification);
+},
+);
