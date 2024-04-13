70a7ed92d16c7d989481a83353b26f7529edfb60
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2024-04-13 20:10:18 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,58 @@
 'use strict';
+
+const setMessage = (text, className) => {
+  const newDiv = document.createElement('div');
+
+  newDiv.dataset.qa = 'notification';
+  newDiv.classList.add(className);
+  newDiv.innerText = text;
+
+  document.body.append(newDiv);
+};
+
+const promise1 = new Promise((resolve, reject) => {
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      resolve('First promise was resolved');
+    }
+  });
+
+  setTimeout(() => reject(new Error('First promise was rejected')), 3000);
+});
+
+promise1
+  .then((success) => setMessage(success, 'success'))
+  .catch((error) => setMessage(error, 'error'));
+
+const promise2 = new Promise((resolve, reject) => {
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      resolve('Second promise was resolved');
+    }
+
+    resolve('Second promise was resolved');
+  });
+});
+
+promise2.then((success) => setMessage(success, 'success'));
+
+const promise3 = new Promise((resolve, reject) => {
+  let right = false;
+  let left = false;
+
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      left = true;
+    }
+
+    if (e.button === 2) {
+      right = true;
+    }
+
+    if (right && left) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+promise3.then((success) => setMessage(success, 'success'));
