724040d4691125d2174ea227c3912b079eec5ec1
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-04-23 14:53:09 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,55 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener('mousedown', (evnt) => {
+    if (evnt.button === 0 || evnt.button === 2) {
+      resolve('Second promise was resolved');
+    }
+  });
+});
+
+const thirdPromise = new Promise((resolve) => {
+  let leftClick = false;
+  let rightClick = false;
+
+  document.addEventListener('mousedown', (evnt) => {
+    if (evnt.button === 0) {
+      leftClick = true;
+    }
+
+    if (evnt.button === 2) {
+      rightClick = true;
+    }
+
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+function createMesage(mesage, type) {
+  const div = document.createElement('div');
+
+  div.classList = type;
+  div.dataset.qa = 'notification';
+  div.innerText = mesage;
+  document.body.append(div);
+}
+
+firstPromise
+  .then((result) => createMesage(result, 'success'))
+  .catch((result) => createMesage(result, 'error'));
+
+secondPromise.then((result) => createMesage(result, 'success'));
+
+thirdPromise.then((result) => createMesage(result, 'success'));
