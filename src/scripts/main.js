e45789fb32b7177530e535b5285b6540cf1f5c84
Rostuk Dudchak <rostikdudchak15@gmail.com>
2024-06-12 21:47:49 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,78 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  const handleClick = (e) => {
+    if (e.button === 0) {
+      resolve('First promise was resolved');
+      logo.removeEventListener('click', handleClick);
+    }
+  };
+
+  logo.addEventListener('click', handleClick);
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+    logo.removeEventListener('click', handleClick);
+  }, 3000);
+});
+
+firstPromise.then(
+  (success) => {
+    const divResolveMessage = document.createElement('div');
+
+    document.body.appendChild(divResolveMessage);
+    divResolveMessage.setAttribute('data-qa', 'notification');
+    divResolveMessage.textContent = success.message;
+  },
+  (error) => {
+    const divErrorMessage = document.createElement('div');
+
+    document.body.appendChild(divErrorMessage);
+    divErrorMessage.setAttribute('data-qa', 'notification');
+    divErrorMessage.textContent = error.message;
+  },
+);
+
+const secondPromise = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  const handleClick = (e) => {
+    if (e.button === 0 || e.button === 2) {
+      resolve('Second promise was resolved');
+      logo.removeEventListener('click', handleClick);
+    }
+  };
+
+  logo.addEventListener('click', handleClick);
+});
+
+secondPromise.then((success) => {
+  const divResolveMessage = document.createElement('div');
+
+  document.body.appendChild(divResolveMessage);
+  divResolveMessage.setAttribute('data-qa', 'notification');
+  divResolveMessage.textContent = success.message;
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  const handleClick = (e) => {
+    if (e.button === 0 && e.button === 2) {
+      resolve('Third promise was resolved');
+      logo.removeEventListener('click', handleClick);
+    }
+  };
+
+  logo.addEventListener('click', handleClick);
+});
+
+thirdPromise.then((success) => {
+  const divResolveMessage = document.createElement('div');
+
+  document.body.appendChild(divResolveMessage);
+  divResolveMessage.setAttribute('data-qa', 'notification');
+  divResolveMessage.textContent = success.message;
+});
