45db455dc791f5a38daed0c1dd10159c302efe9a
Den <clixqqk@gmail.com>
2024-06-20 20:14:07 +0300

    dev

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,107 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+let clickRight = 0;
+let clickLeft = 0;
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    clickLeft++;
+    resolve();
+  });
+
+  setTimeout(() => {
+    reject(new Error('Timeout occurred'));
+  }, 3000);
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    clickLeft++;
+    resolve();
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    clickRight++;
+
+    resolve();
+  });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    if (clickRight > 0 && clickLeft > 0) {
+      resolve();
+    }
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+
+    if (clickRight > 0 && clickLeft > 0) {
+      resolve();
+    }
+  });
+});
+
+firstPromise
+  .then(() => {
+    body.appendChild(createfirstPromiseResolve());
+  })
+  .catch(() => {
+    body.appendChild(createfirstPromiseReject());
+  });
+
+secondPromise.then(() => {
+  body.appendChild(createsecondPromiseResolve());
+});
+
+thirdPromise.then(() => {
+  body.appendChild(createthirdPromiseResolve());
+});
+
+function createfirstPromiseResolve() {
+  const el = document.createElement('div');
+
+  el.setAttribute('data-qa', 'notification');
+  el.classList.add('message');
+  el.classList.add('success');
+  el.textContent = 'First promise was resolved';
+
+  return el;
+}
+
+function createfirstPromiseReject() {
+  const el = document.createElement('div');
+
+  el.setAttribute('data-qa', 'notification');
+  el.classList.add('message');
+  el.classList.add('error');
+  el.textContent = 'First promise was rejected';
+
+  return el;
+}
+
+function createsecondPromiseResolve() {
+  const el = document.createElement('div');
+
+  el.setAttribute('data-qa', 'notification');
+  el.classList.add('message');
+  el.classList.add('success');
+  el.textContent = 'Second promise was resolved';
+
+  return el;
+}
+
+function createthirdPromiseResolve() {
+  const el = document.createElement('div');
+
+  el.setAttribute('data-qa', 'notification');
+  el.classList.add('message');
+  el.classList.add('success');
+  el.textContent = 'Third promise was resolved';
+
+  return el;
+}
