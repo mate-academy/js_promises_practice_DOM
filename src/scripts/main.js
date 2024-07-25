ae76ff6a84211773450d0be095886bcc887d8f0f
luter <newgglutt@gmail.com>
2024-07-25 13:28:18 +0300

    sdsa1szczecvhjj

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,39 +2,53 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   let leftClicked = false;
+  let firstPromiseResolved = false;
 
   const firstPromise = new Promise((resolve, reject) => {
     document.addEventListener('click', (e) => {
-      if (e.target.classList.contains('logo')) {
+      if (e.clientX < window.innerWidth / 2 && !firstPromiseResolved) {
+        firstPromiseResolved = true;
         resolve('First promise was resolved');
       }
     });
 
     setTimeout(() => {
-      reject(new Error('First promise was rejected'));
+      if (!firstPromiseResolved) {
+        reject(new Error('First promise was rejected'));
+      }
     }, 3000);
   });
 
   const secondPromise = new Promise((resolve) => {
-    document.addEventListener('click', (e) => {
-      if (e.clientX < window.innerWidth / 2) {
-        leftClicked = true;
-        resolve('Second promise was resolved');
-      }
-    }, { once: true });
+    document.addEventListener(
+      'click',
+      (e) => {
+        if (e.clientX < window.innerWidth / 2) {
+          leftClicked = true;
+          resolve('Second promise was resolved');
+        }
+      },
+      { once: true },
+    );
   });
 
   const thirdPromise = new Promise((resolve) => {
-    document.addEventListener('contextmenu', (e) => {
-      e.preventDefault();
-      if (leftClicked && e.clientX > window.innerWidth / 2) {
-        resolve('Third promise was resolved');
-      }
-    }, { once: true });
+    document.addEventListener(
+      'contextmenu',
+      (e) => {
+        e.preventDefault();
+
+        if (leftClicked && e.clientX > window.innerWidth / 2) {
+          resolve('Third promise was resolved');
+        }
+      },
+      { once: true },
+    );
   });
 
   const handleSuccess = (message) => {
     const notification = document.createElement('div');
+
     notification.className = 'message success';
     notification.setAttribute('data-qa', 'notification');
     notification.textContent = message;
@@ -47,6 +61,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const handleError = (error) => {
     const notification = document.createElement('div');
+
     notification.className = 'message error';
     notification.setAttribute('data-qa', 'notification');
     notification.textContent = error.message;
