c981bb4e8fdc1dcd1a8e59b76cd989cd23920234
Andrew <beliyandr@gmail.com>
2024-07-27 22:57:01 +0300

    firts Dom promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,84 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+firstPromise
+  .then((data) => {
+    showPositiveResult(data);
+  })
+  .catch((info) => {
+    showNegativeResult(info);
+  });
+
+const secondPromise = new Promise(function (resolve) {
+  document.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    resolve('Second promise was resolved');
+  });
+});
+
+secondPromise.then((data) => {
+  showPositiveResult(data);
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  let leftClick = false;
+  let rigthClick = false;
+
+  document.addEventListener('click', () => {
+    leftClick = true;
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    rigthClick = true;
+
+    if (leftClick && rigthClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+thirdPromise.then((data) => {
+  showPositiveResult(data);
+});
+
+function showNegativeResult(result) {
+  const dataHtml = document.createElement('div');
+
+  dataHtml.dataset.qa = 'notification';
+  dataHtml.classList.add('error');
+  dataHtml.textContent = result;
+
+  document.body.append(dataHtml);
+
+  setTimeout(() => {
+    dataHtml.remove();
+  }, 3000);
+}
+
+function showPositiveResult(result) {
+  const dataHtml = document.createElement('div');
+
+  dataHtml.dataset.qa = 'notification';
+  dataHtml.classList.add('success');
+  dataHtml.textContent = result;
+
+  document.body.append(dataHtml);
+
+  setTimeout(() => {
+    dataHtml.remove();
+  }, 3000);
+}
