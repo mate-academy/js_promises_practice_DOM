b503ecb12d4fd7484cb7727fe666233c176bfbcc
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-07-29 18:50:50 +0300

    add first promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+new Promise((resolve, reject) => {
+  let isClicked = false;
+
+  document.addEventListener('click', () => {
+    isClicked = true;
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    if (!isClicked) {
+      reject(new Error('First promise was rejected'));
+    }
+  }, 3000);
+})
+  .then((message) => {
+    document.body.insertAdjacentHTML(
+      'beforeend',
+      `<div data-qa="notification" class="success">${message}</div>`,
+    );
+  })
+  .catch((message) => {
+    document.body.insertAdjacentHTML(
+      'beforeend',
+      `<div data-qa="notification" class="error">${message}</div>`,
+    );
+  });
