7c7857e886f468a6430e72c6c31e3bf2b370be5f
Radymyr <loboda.radymyr@gmail.com>
2024-08-06 00:24:38 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,61 +3,63 @@
 
 const eventsType = ['contextmenu', 'click'];
 
-const showSuccessNotification = (successText) => {
+const showNotification = (someData) => {
   const div = document.createElement('div');
 
-  div.classList.add('success');
-  div.setAttribute('data-qa', 'notification');
-  div.textContent = successText;
-  document.body.append(div);
-};
+  if (someData.success) {
+    div.classList.add('success');
+  }
 
-const showErrorNotification = (errorText) => {
-  const div = document.createElement('div');
+  if (!someData.success) {
+    div.classList.add('error');
+  }
 
-  div.classList.add('error');
   div.setAttribute('data-qa', 'notification');
-  div.textContent = errorText;
+  div.textContent = someData.message;
+
   document.body.append(div);
 };
 
 const firstPromise = new Promise((resolve, reject) => {
   document.addEventListener(eventsType[1], () => {
-    resolve('First promise was resolved');
+    resolve({ success: true, message: 'First promise was resolved' });
   });
 
-  setTimeout(reject, 3000, 'First promise was rejected ');
+  setTimeout(reject, 3000, {
+    success: false,
+    message: 'First promise was rejected',
+  });
 });
 
 firstPromise
-  .then((successText) => showSuccessNotification(successText))
-  .catch((errorText) => showErrorNotification(errorText));
+  .then((successData) => showNotification(successData))
+  .catch((errorData) => showNotification(errorData));
 
 const secondPromise = new Promise((resolve) => {
   eventsType.forEach((eventType) => {
     document.addEventListener(eventType, () => {
-      resolve('Second promise was resolved');
+      resolve({ success: true, message: 'Second promise was resolved' });
     });
   });
 });
 
-secondPromise.then((successText) => showSuccessNotification(successText));
+secondPromise.then((successData) => showNotification(successData));
 
 const thirdPromise = () => {
   const promise1 = new Promise((resolve) => {
     document.addEventListener(eventsType[0], () => {
-      resolve('Third promise was resolved');
+      resolve({ success: true, message: 'Third promise was resolved' });
     });
   });
 
   const promise2 = new Promise((resolve) => {
     document.addEventListener(eventsType[1], () => {
-      resolve('Third promise was resolved');
+      resolve({ success: true, message: 'Third promise was resolved' });
     });
   });
 
-  Promise.all([promise1, promise2]).then((successText) =>
-    showSuccessNotification(successText),
+  Promise.all([promise1, promise2]).then((successData) =>
+    showNotification(successData[0]),
   );
 };
 
