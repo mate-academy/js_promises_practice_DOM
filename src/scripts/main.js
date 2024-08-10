0ab8781b9838d8b585c5fa68e6d431b213d06063
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2024-08-10 05:09:43 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,68 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+function notification(type, textMessage) {
+  const newDiv = document.createElement('div');
+
+  newDiv.setAttribute('data-qa', 'notification');
+  newDiv.className = type;
+  newDiv.textContent = textMessage;
+  body.append(newDiv);
+}
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve();
+  });
+  setTimeout(() => reject(new Error()), 3000);
+});
+
+firstPromise
+  .then(() => {
+    notification('success', 'First promise was resolved');
+  })
+  .catch(() => {
+    notification('error', 'First promise was rejected');
+  });
+
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener('mousedown', () => {
+    resolve();
+  });
+});
+
+secondPromise
+  .then(() => {
+    notification('success', 'Second promise was resolved');
+  })
+  .catch(() => {
+    notification('error', 'Second promise was rejected');
+  });
+
+const thirdPromise = new Promise((resolve) => {
+  let leftClick = false;
+  let rightClick = false;
+
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      leftClick = true;
+    }
+
+    if (e.button === 2) {
+      rightClick = true;
+    }
+
+    if (leftClick && rightClick) {
+      resolve();
+    }
+  });
+});
+
+thirdPromise
+  .then(() => {
+    notification('success', 'Third promise was resolved');
+  })
+  .catch(() => {
+    notification('error', 'Third promise was rejected');
+  });
