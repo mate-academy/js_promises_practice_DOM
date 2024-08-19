5093398c882b4c226df74e6af86f975912fbcd7a
Iryna Liashko <ira.liashko1@gmail.com>
2024-08-19 21:31:04 +0200

    have questions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,66 @@
 'use strict';
+
+const addClass = function (className, text) {
+  const tag = document.createElement('div');
+
+  tag.setAttribute('data-qa', 'notification');
+  tag.className = className;
+  tag.innerHTML = text;
+  document.body.appendChild(tag);
+};
+
+const promise1 = new Promise((resolve, reject) => {
+  const timeout = setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+
+  document.addEventListener('click', function (e) {
+    if (e.button === 0) {
+      clearTimeout(timeout);
+      resolve('First promise was resolved');
+    }
+  });
+});
+
+promise1
+  .then((message) => {
+    addClass('message success-message', message);
+  })
+  .catch((err) => {
+    addClass('message error-message', err.message);
+  });
+
+const promise2 = new Promise((resolve) => {
+  document.addEventListener('click', function (e) {
+    if (e.button === 0 || e.button === 2) {
+      resolve('Second promise was resolved');
+    }
+  });
+});
+
+promise2.then((message) => {
+  addClass('message', message);
+});
+
+let leftClick = false;
+let rightClick = false;
+
+const promise3 = new Promise((resolve) => {
+  document.addEventListener('click', function (e) {
+    if (e.button === 0) {
+      leftClick = true;
+    }
+
+    if (e.button === 2) {
+      rightClick = true;
+    }
+
+    if (rightClick && leftClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+promise3.then((message) => {
+  addClass('message success-message', message);
+});
