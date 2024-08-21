4e3434d6f7e734f7f189e660f35baf02af337221
Vlados <vnegoluk@gmail.com>
2024-08-21 15:39:55 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,68 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+function createNotification(type, textMessage) {
+  const notification = document.createElement('div');
+
+  notification.setAttribute('data-qa', 'notification');
+  notification.className = type;
+  notification.textContent = textMessage;
+  body.append(notification);
+}
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve();
+  });
+  setTimeout(() => reject(new Error()), 3000);
+});
+
+firstPromise
+  .then(() => {
+    createNotification('succes', 'First promise was resolved');
+  })
+  .catch(() => {
+    createNotification('error', 'First promise was rejected');
+  });
+
+const secondPromise = new Promise((resolve, reject) => {
+  document.addEventListener('mousedown', () => {
+    resolve();
+  });
+});
+
+secondPromise
+  .then(() => {
+    createNotification('succes', 'Second promise was resolved');
+  })
+  .catch(() => {
+    createNotification('error', 'Seconsd promise was rejected');
+  });
+
+const thirdPromise = new Promise((resolve, reject) => {
+  let mouseLeft = false;
+  let mouseRight = false;
+
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      mouseLeft = true;
+    }
+
+    if (e.button === 2) {
+      mouseRight = true;
+    }
+
+    if (mouseLeft && mouseRight) {
+      resolve();
+    }
+  });
+});
+
+thirdPromise
+  .then(() => {
+    createNotification('succes', 'Third promise was resolved');
+  })
+  .catch(() => {
+    createNotification('error', 'Third promise was rejected');
+  });
