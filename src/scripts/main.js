8289a1b7a244ac80306b076f54d1533377cc80bd
Jakub Bajkowski <k.bajkowski7@wp.pl>
2024-09-02 07:15:29 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,59 +1,59 @@
 const body = document.querySelector('body');
 
 function firstPromise() {
-  try {
+  return new Promise((resolve, reject) => {
     let a = 0;
+
     const timeOut = setTimeout(() => {
       const errorMessage =
         '<div data-qa="notification" class="error">' +
         'First promise was rejected</div>';
 
       body.insertAdjacentHTML('afterbegin', errorMessage);
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject('First promise was rejected');
       a = 2;
     }, 3000);
 
     document.addEventListener('click', (e) => {
-      if (e.button === 0) {
-        a += 1;
+      if (e.button === 0 && a === 0) {
+        a = 1;
 
-        if (a === 1) {
-          const successMessage =
-            '<div data-qa="notification" class="success">' +
-            'First promise was resolved</div>';
+        const successMessage =
+          '<div data-qa="notification" class="success">' +
+          'First promise was resolved</div>';
 
-          body.insertAdjacentHTML('afterbegin', successMessage);
-          clearTimeout(timeOut);
-        }
+        body.insertAdjacentHTML('afterbegin', successMessage);
+        clearTimeout(timeOut);
+        resolve('First promise was resolved');
         a = 2;
       }
     });
-  } catch (error) {}
+  });
 }
 
 function secondPromise() {
-  try {
+  return new Promise((resolve) => {
     let a = 0;
 
     document.addEventListener('mousedown', (e) => {
-      if (e.button === 0 || e.button === 2) {
+      if ((e.button === 0 || e.button === 2) && a === 0) {
         a = 1;
 
-        if (a === 1) {
-          const successMessage =
-            '<div data-qa="notification" class="success">' +
-            'Second promise was resolved</div>';
+        const successMessage =
+          '<div data-qa="notification" class="success">' +
+          'Second promise was resolved</div>';
 
-          body.insertAdjacentHTML('afterbegin', successMessage);
-
-          a = 2;
-        }
+        body.insertAdjacentHTML('afterbegin', successMessage);
+        resolve('Second promise was resolved');
+        a = 2;
       }
     });
-  } catch (error) {}
+  });
 }
 
 function thirdPromise() {
-  try {
+  return new Promise((resolve) => {
     let a = 0;
     let b = 0;
 
@@ -72,13 +72,16 @@ function thirdPromise() {
           'Third promise was resolved</div>';
 
         body.insertAdjacentHTML('afterbegin', successMessage);
-
-        a = 2;
+        resolve('Third promise was resolved');
+        a = b = 2;
       }
     });
-  } catch (error) {}
+  });
 }
 
-firstPromise();
-secondPromise();
-thirdPromise();
+// eslint-disable-next-line no-console
+firstPromise().catch((error) => console.error(error));
+// eslint-disable-next-line no-console
+secondPromise().then((message) => console.log(message));
+// eslint-disable-next-line no-console
+thirdPromise().then((message) => console.log(message));
