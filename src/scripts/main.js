2fb09fe2f4901254cd7df136bc914644cc8248fc
Dmytro Ivasenko <dmtivasenko@gmail.com>
2024-09-08 09:43:42 +0700

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,47 @@
 'use strict';
+
+// Create 3 promises:
+//   - The `firstPromise`
+function createDiv(result, classType) {
+  const div = document.createElement('div');
+
+  div.classList.add(classType);
+  div.dataset.qa = 'notification';
+  div.textContent = result;
+  document.body.append(div);
+}
+
+const promise1 = new Promise((resolve, reject) => {
+  document.body.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => reject(new Error('First promise was rejected')), 3000);
+});
+
+promise1
+  .then((result) => createDiv(result, 'success'))
+  .catch((error) => createDiv(error, 'error'));
+
+// - The `secondPromise`
+
+const left = new Promise((resolve) => {
+  document.body.addEventListener('click', () => {
+    resolve('left');
+  });
+});
+
+const right = new Promise((resolve) => {
+  document.body.addEventListener('contextmenu', () => {
+    resolve('right');
+  });
+});
+
+Promise.any([left, right]).then((result) => {
+  createDiv('Second promise was resolved', 'success');
+});
+
+// - The `thirdPromise`
+Promise.all([left, right]).then((result) => {
+  createDiv('Third promise was resolved', 'success');
+});
