8ab1b139a6a990fd93c9ec9e1b1cfc371af92a79
Vitaliy <vitaliklevytskyi07@gmail.com>
2024-09-19 16:12:43 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,64 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+const createNotification = (message, type) => {
+  const notification = document.createElement('div');
+
+  notification.setAttribute('data-qa', 'notification');
+  notification.className = type;
+  notification.textContent = message;
+  body.appendChild(notification);
+};
+
+const firstPromise = new Promise((resolve, reject) => {
+  const timer = setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+
+  document.addEventListener('click', () => {
+    clearTimeout(timer);
+    resolve('First promise was resolved');
+  });
+});
+
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    resolve('Second promise was resolved');
+  });
+});
+
+let leftClick = false;
+let rightClick = false;
+
+const thirdPromise = new Promise((resolve) => {
+  const checkBothClicks = () => {
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  };
+
+  document.addEventListener('click', () => {
+    leftClick = true;
+    checkBothClicks();
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    rightClick = true;
+    checkBothClicks();
+  });
+});
+
+firstPromise
+  .then((message) => createNotification(message, 'success'))
+  .catch((error) => createNotification(error, 'error'));
+
+secondPromise.then((message) => createNotification(message, 'success'));
+
+thirdPromise.then((message) => createNotification(message, 'success'));
