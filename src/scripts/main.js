3c0b3e9dc827c554ce3a5c7d5e3c32697c0d1eb1
associate2coder <olatsko@gmail.com>
2024-10-05 00:30:18 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,59 @@
 'use strict';
+
+const handlePromise = (className, message) => {
+  const element = document.createElement('div');
+
+  element.setAttribute('data-qa', 'notification');
+  element.classList.add(className);
+  element.textContent = message;
+  document.body.append(element);
+};
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    reject('First promise was rejected');
+  }, 3000);
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  });
+
+  document.addEventListener('contextmenu', (mouseEvent) => {
+    mouseEvent.preventDefault();
+    resolve('Second promise was resolved');
+  });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  let leftClick = false;
+  let rightClick = false;
+  const resolveThirdPromise = () => {
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  };
+
+  document.addEventListener('click', () => {
+    leftClick = true;
+    resolveThirdPromise();
+  });
+
+  document.addEventListener('contextmenu', (mouseEvent) => {
+    mouseEvent.preventDefault();
+    rightClick = true;
+    resolveThirdPromise();
+  });
+});
+
+[firstPromise, secondPromise, thirdPromise].forEach((promise) => {
+  promise
+    .then((message) => handlePromise('success', message))
+    .catch((message) => handlePromise('error', message));
+});
