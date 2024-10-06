8fd00744065a09658b9a5a58d1dc08bdbdba843d
Viktoriia Dereza <viktoriiadereza7@gmail.com>
2024-10-06 19:12:05 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,72 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const notification = (message, isSuccess) => {
+    const div = document.createElement('div');
+
+    div.setAttribute('data-qa', 'notification');
+    div.className = isSuccess ? 'success' : 'error';
+    div.textContent = message;
+    document.body.appendChild(div);
+  };
+
+  // Перший проміс
+  const firstPromise = new Promise((resolve, reject) => {
+    document.addEventListener('click', (ev) => {
+      if (ev.button === 0) {
+        resolve('First promise was resolved');
+      }
+    });
+
+    setTimeout(() => {
+      reject(new Error('First promise was rejected'));
+    }, 3000);
+  });
+
+  // Другий проміс
+  const secondPromise = new Promise((resolve) => {
+    document.addEventListener(
+      'click',
+      (ev) => {
+        if (ev.button === 0 || ev.button === 2) {
+          resolve('Second promise was resolved');
+        }
+      },
+      { once: true },
+    );
+  });
+
+  // Третій проміс
+  const thirdPromise = new Promise((resolve) => {
+    let leftClickHappened = false;
+    let rightClickHappened = false;
+
+    const handleThirdClick = (ev) => {
+      if (ev.button === 0) {
+        leftClickHappened = true;
+      } else if (ev.button === 2) {
+        rightClickHappened = true;
+      }
+
+      if (leftClickHappened && rightClickHappened) {
+        resolve('Third promise was resolved after both left and right clicks');
+        document.removeEventListener('click', handleThirdClick);
+      }
+    };
+
+    document.addEventListener('click', handleThirdClick);
+  });
+
+  firstPromise
+    .then((message) => notification(message, true))
+    .catch((err) => notification(err.message, false));
+
+  secondPromise.then((message) => notification(message, true));
+
+  thirdPromise.then((message) => notification(message, true));
+
+  // Відключаємо контекстне меню
+  document.addEventListener('contextmenu', (ev) => {
+    ev.preventDefault();
+  });
+});
