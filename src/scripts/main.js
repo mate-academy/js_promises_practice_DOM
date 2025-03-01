cb7f06c8b55cf4655db0159731b6ef87fa093f3b
Grzegorz <grze.pra@gmail.com>
2025-03-01 13:33:46 +0100

    Second and third promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,14 +6,43 @@ const firstPromise = new Promise((resolve, reject) => {
 
   body.addEventListener('click', (click) => {
     clicked = true;
-    resolve('First promise was resolved!');
+    resolve('First promise was resolved');
   });
 
   setTimeout(() => {
     if (!clicked) {
-      reject(new Error('First promise was rejected!'));
+      reject(new Error('First promise was rejected'));
     }
   }, 3000);
+});
+
+const secondPromise = new Promise((resolve) => {
+  const body = document.querySelector('body');
+
+  body.addEventListener('mousedown', (click) => {
+    if (click.button === 0 || click.button === 2) {
+      resolve('Second promise was resolved');
+    }
+  });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  const body = document.querySelector('body');
+
+  let leftClick = false;
+  let rightClick = false;
+
+  body.addEventListener('mousedown', (click) => {
+    if (click.button === 0) {
+      leftClick = true;
+    } else if (click.button === 2) {
+      rightClick = true;
+    }
+
+    if (leftClick === true && rightClick === true) {
+      resolve('Third promise was resolved');
+    }
+  });
 });
 
 firstPromise
@@ -22,6 +51,7 @@ firstPromise
 
     success.classList.add('success');
     success.setAttribute('data-qa', 'notification');
+    success.textContent = message;
 
     document.body.appendChild(success);
   })
@@ -30,6 +60,27 @@ firstPromise
 
     error.classList.add('error');
     error.setAttribute('data-qa', 'notification');
+    error.textContent = message;
 
     document.body.appendChild(error);
   });
+
+secondPromise.then((message) => {
+  const success = document.createElement('div');
+
+  success.classList.add('success');
+  success.setAttribute('data-qa', 'notification');
+  success.textContent = message;
+
+  document.body.appendChild(success);
+});
+
+thirdPromise.then((message) => {
+  const success = document.createElement('div');
+
+  success.classList.add('success');
+  success.setAttribute('data-qa', 'notification');
+  success.textContent = message;
+
+  document.body.appendChild(success);
+});
