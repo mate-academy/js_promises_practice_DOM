7016b7b6abb4b5e66676c5bd5b4e81a54c1f1a4e
troleksii <aliosha123418@gmail.com>
2025-03-14 21:15:57 +0100

    error message fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,65 +13,62 @@ const firstPromise = new Promise((resolve, reject) => {
   }, 3000);
 });
 
-
 firstPromise
-.then(() => {
+  .then(() => {
     messageConstructor(`First promise was resolved`);
   })
   .catch((error) => {
     messageConstructor(error.message, true);
   });
 
-  // SECOND PROMISE
+// SECOND PROMISE
 
-  const secondPromise = new Promise((resolve) => {
-    document.addEventListener('mousedown', (e) => {
-      if (e.button === 0 || e.button === 2) {
-        resolve();
-      }
-    });
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0 || e.button === 2) {
+      resolve();
+    }
   });
+});
 
-  secondPromise.then(() => {
-    messageConstructor('Second promise was resolved');
-  });
+secondPromise.then(() => {
+  messageConstructor('Second promise was resolved');
+});
 
-  // THIRD PROMISE
+// THIRD PROMISE
 
-  const thirdPromise = new Promise((resolve) => {
-    let leftClicked = false;
-    let rightClicked = false;
+const thirdPromise = new Promise((resolve) => {
+  let leftClicked = false;
+  let rightClicked = false;
 
-    document.addEventListener('mousedown', (e) => {
-      if (e.button === 0) {
-        leftClicked = true;
-      }
-
-      if (e.button === 2) {
-        rightClicked = true;
-      }
-
-      if (leftClicked && rightClicked) {
-        resolve();
-      }
-    });
-  });
-
-    // messageConstructor
-
-  function messageConstructor(text, isError = false) {
-    const message = document.createElement('div');
-
-    message.setAttribute('data-qa', 'notification');
-    message.classList.add('success');
-
-    if (isError) {
-      message.classList.add('error');
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      leftClicked = true;
     }
 
-    message.textContent = text;
-    document.body.append(message);
-  }
+    if (e.button === 2) {
+      rightClicked = true;
+    }
+
+    if (leftClicked && rightClicked) {
+      resolve();
+    }
+  });
+});
+
+// messageConstructor
+
+function messageConstructor(text, isError = false) {
+  const message = document.createElement('div');
+
+  message.setAttribute('data-qa', 'notification');
+
+  message.classList.add(isError ? 'error' : 'success');
+
+  message.textContent = text;
+  document.body.append(message);
+}
+
 thirdPromise.then(() => {
   messageConstructor('Third promise was resolved');
 });
