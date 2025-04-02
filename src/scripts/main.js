9276425e1b11a8bb0291b7d21c5477fc6471f160
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2025-04-02 19:05:42 +0400

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,63 @@
 'use strict';
+
+function createNotification(message, isError = false) {
+  const div = document.createElement('div');
+
+  div.dataset.qa = 'notification';
+  div.className = isError ? 'error' : 'success';
+  div.textContent = message;
+  document.body.appendChild(div);
+}
+
+const firstPromise = new Promise((resolve, reject) => {
+  const timer = setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+
+  document.addEventListener('click', (e) => {
+    if (e.button === 0) {
+      clearTimeout(timer);
+      resolve('First promise was resolved');
+    }
+  });
+});
+
+firstPromise
+  .then((msg) => createNotification(msg))
+  .catch((err) => createNotification(err.message, true));
+
+const secondPromise = new Promise((resolve) => {
+  const handler = (e) => {
+    if (e.button === 0 || e.button === 2) {
+      document.removeEventListener('click', handler);
+      resolve('Second promise was resolved');
+    }
+  };
+
+  document.addEventListener('click', handler);
+});
+
+secondPromise.then((msg) => createNotification(msg));
+
+let leftClicked = false;
+let rightClicked = false;
+
+const thirdPromise = new Promise((resolve) => {
+  document.addEventListener('click', (e) => {
+    if (e.button === 0) {
+      leftClicked = true;
+    }
+
+    if (e.button === 2) {
+      rightClicked = true;
+    }
+
+    if (leftClicked && rightClicked) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+thirdPromise.then((msg) => createNotification(msg));
+
+document.addEventListener('contextmenu', (e) => e.preventDefault());
