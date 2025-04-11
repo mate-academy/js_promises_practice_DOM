a5d2458c6f16d933adac731145b4d14871c9e14e
Iryna <kapustyak.ira97@gmail.com>
2025-04-11 19:17:09 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,62 @@
 'use strict';
+
+const onSuccess = (message) => {
+  const divEl = document.createElement('div');
+  divEl.className = 'success';
+  divEl.textContent = message;
+  document.body.append(divEl);
+};
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject('First promise was rejected');
+  }, 3000);
+});
+
+firstPromise.then(onSuccess);
+
+firstPromise.catch((message) => {
+  const divEl = document.createElement('div');
+  divEl.className = 'error';
+  divEl.textContent = message;
+  document.body.append(divEl);
+});
+
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  });
+
+  document.addEventListener('contextmenu', () => {
+    resolve('Second promise was resolved');
+  });
+});
+
+secondPromise.then(onSuccess);
+
+const thirdPromise = new Promise((resolve) => {
+  let wasClickedOnLeft;
+  let wasClickedOnRight;
+
+  document.addEventListener('click', () => {
+    wasClickedOnLeft = true;
+
+    if (wasClickedOnLeft && wasClickedOnRight === true) {
+      resolve('Third promise was resolved');
+    }
+  });
+
+  document.addEventListener('contextmenu', () => {
+    wasClickedOnRight = true;
+
+    if (wasClickedOnLeft && wasClickedOnRight === true) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+thirdPromise.then(onSuccess);
