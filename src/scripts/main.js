6f31d386b75c7a3159a43366f236bdcbf552043f
i-melnyk <i.melnyk1985@gmail.com>
2025-04-27 19:39:52 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,80 @@
 'use strict';
+
+let resolveFirstPromise;
+const firstPromise = new Promise((resolve, reject) => {
+  resolveFirstPromise = resolve;
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+firstPromise.then(success).catch((err) => error(err.message));
+
+let leftClick = false;
+let rightClick = false;
+
+let resolveSecondPromise;
+const secondPromise = new Promise((resolve) => {
+  resolveSecondPromise = resolve;
+});
+
+secondPromise.then(success);
+
+const thirdPromise = new Promise((resolve, reject) => {
+  resolve('Third promise was resolved');
+});
+
+document.addEventListener('mousedown', (e) => {
+  if (e.button === 0) {
+    if (resolveFirstPromise) {
+      resolveFirstPromise('First promise was resolved');
+      resolveFirstPromise = null;
+    }
+
+    if (resolveSecondPromise) {
+      resolveSecondPromise('Second promise was resolved');
+      resolveSecondPromise = null;
+    }
+
+    leftClick = true;
+
+    if (rightClick) {
+      thirdPromise.then((message) => success(message));
+      leftClick = false;
+      rightClick = false;
+    }
+  }
+
+  if (e.button === 2) {
+    if (resolveSecondPromise) {
+      resolveSecondPromise('Second promise was resolved');
+      resolveSecondPromise = null;
+    }
+    rightClick = true;
+
+    if (leftClick) {
+      thirdPromise.then((message) => success(message));
+      leftClick = false;
+      rightClick = false;
+    }
+  }
+});
+
+function success(message) {
+  const el = document.createElement('div');
+
+  el.setAttribute('data-qa', 'notification');
+  el.classList.add('success');
+  el.textContent = message;
+  document.body.appendChild(el);
+}
+
+function error(message) {
+  const el = document.createElement('div');
+
+  el.setAttribute('data-qa', 'notification');
+  el.classList.add('error');
+  el.textContent = message;
+  document.body.appendChild(el);
+}
