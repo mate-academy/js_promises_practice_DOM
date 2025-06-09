e9bd182b72ee0bda15fef126f83629e547f32fd5
Tetiana Chuhai <chugaytatyana@gmail.com>
2025-06-09 19:38:24 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,77 @@
 'use strict';
+
+function setNotification(type, message) {
+  const notification = document.createElement('div');
+
+  notification.className = type === 'error' ? 'error' : 'success';
+  notification.setAttribute('data-qa', 'notification');
+  notification.textContent = message;
+
+  document.body.appendChild(notification);
+}
+
+const firstPromise = new Promise((resolve, reject) => {
+  let resolved = false;
+
+  function getValue(e) {
+    if (e.button === 0) {
+      resolved = true;
+      resolve('First promise was resolved');
+      document.removeEventListener('click', getValue);
+    }
+  }
+
+  document.addEventListener('click', getValue);
+
+  setTimeout(() => {
+    if (!resolved) {
+      reject(new Error('First promise was rejected'));
+      document.removeEventListener('click', getValue);
+    }
+  }, 3000);
+});
+
+firstPromise
+  .then((msg) => setNotification('success', msg))
+  .catch((err) => setNotification('error', err));
+
+const secondPromise = new Promise((resolve) => {
+  function getPromiseValue(e) {
+    if (e.button === 0 || e.button === 2) {
+      resolve('Second promise was resolved');
+      document.removeEventListener('mousedown', getPromiseValue);
+    }
+  }
+
+  document.addEventListener('mousedown', getPromiseValue);
+});
+
+secondPromise.then((msg) => setNotification('success', msg));
+
+const thirdPromise = new Promise((resolve) => {
+  let leftClicked = false;
+  let rightClicked = false;
+
+  function getValue(e) {
+    if (e.button === 0) {
+      leftClicked = true;
+    }
+
+    if (e.button === 2) {
+      rightClicked = true;
+    }
+
+    if (leftClicked && rightClicked) {
+      resolve('Third promise was resolved');
+      document.removeEventListener('mousedown', getValue);
+    }
+  }
+
+  document.addEventListener('mousedown', getValue);
+});
+
+thirdPromise.then((msg) => setNotification('success', msg));
+
+document.addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+});
