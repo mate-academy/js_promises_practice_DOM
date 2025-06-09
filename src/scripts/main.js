a88f52958634647e82b68b596eb9fefa8b3e84a5
Orest Vaheman <ovaheman@gmail.com>
2025-06-09 21:46:07 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,77 @@
 'use strict';
+
+const doc = document.querySelector('html');
+
+const firstPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+
+  doc.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+firstPromise.then(() => {
+  const div = document.createElement('div');
+
+  div.setAttribute('data-qa', 'notification');
+  div.textContent = 'First promise was resolved';
+  div.className = 'success';
+
+  document.body.appendChild(div);
+});
+
+firstPromise.catch(() => {
+  const div = document.createElement('div');
+
+  div.setAttribute('data-qa', 'notification');
+  div.textContent = 'First promise was rejected';
+  div.className = 'error';
+
+  document.body.appendChild(div);
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  doc.addEventListener('mousedown', () => {
+    resolve();
+  });
+});
+
+secondPromise.then(() => {
+  const div = document.createElement('div');
+
+  div.setAttribute('data-qa', 'notification');
+  div.textContent = 'Second promise was resolved';
+  div.className = 'success';
+  document.body.appendChild(div);
+});
+
+const thirdPromise = new Promise((resolve) => {
+  const leftClickDone = { done: false };
+  const rightClickDone = { done: false };
+
+  doc.addEventListener('mousedown', (clk) => {
+    if (clk.button === 0) {
+      leftClickDone.done = true;
+    }
+
+    if (clk.button === 2) {
+      rightClickDone.done = true;
+    }
+
+    if (leftClickDone.done && rightClickDone.done) {
+      resolve();
+    }
+  });
+});
+
+thirdPromise.then(() => {
+  const div = document.createElement('div');
+
+  div.setAttribute('data-qa', 'notification');
+  div.textContent = 'Third promise was resolved';
+  div.className = 'success';
+
+  document.body.appendChild(div);
+});
