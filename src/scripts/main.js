cbf64dd494c86dea075a6b9d0d879681038a6987
Maciej <metro1568@gmail.com>
2025-06-11 13:35:29 +0200

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,12 +3,18 @@
 
 let promise1Resolve;
 let promise2Resolve;
-let promise3ResolvePartOne;
-let promise3ResolvePartTwo;
+let promise3Resolve;
+
 const whichClick = {
   left: false,
   right: false,
 };
+
+function checkPromise3() {
+  if (whichClick.left && whichClick.right) {
+    promise3Resolve('Third promise was resolved');
+  }
+}
 
 function createNotification(className, message) {
   const div = document.createElement('div');
@@ -44,13 +50,8 @@ const secondPromise = new Promise((resolve, reject) => {
 
 // eslint-disable-next-line no-unused-vars
 const thirdPromise = new Promise((resolve, reject) => {
-  promise3ResolvePartOne = resolve;
+  promise3Resolve = resolve;
 })
-  .then(async () => {
-    return new Promise((resolve, reject) => {
-      promise3ResolvePartTwo = resolve;
-    });
-  })
   .then((message) => createNotification('success', message))
   .catch((message) => createNotification('error', message));
 
@@ -58,19 +59,11 @@ document.addEventListener('click', (e) => {
   whichClick.left = true;
   promise1Resolve(`First promise was resolved`);
   promise2Resolve('Second promise was resolved');
-  promise3ResolvePartOne();
-
-  if (whichClick.right === true) {
-    promise3ResolvePartTwo('Third promise was resolved');
-  }
+  checkPromise3();
 });
 
 document.addEventListener('contextmenu', (e) => {
   whichClick.right = true;
   promise2Resolve('Second promise was resolved');
-  promise3ResolvePartOne();
-
-  if (whichClick.left === true) {
-    promise3ResolvePartTwo('Third promise was resolved');
-  }
+  checkPromise3();
 });
