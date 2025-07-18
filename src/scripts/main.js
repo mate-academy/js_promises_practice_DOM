78df6f1b6150ea7604a84b7feae402b2d786a313
Alina Yamchuk <alina.utc@gmail.com>
2025-07-18 13:39:01 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,76 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+
+  const timer = setTimeout(() => {
+  reject('First promise was rejected');
+}, 3000);
+
+document.addEventListener('click', (event) => {
+if (event.button === 0) {
+  clearTimeout(timer);
+  resolve ('First promise was resolved');
+}
+
+})
+});
+
+firstPromise
+  .then((message) => {
+    const div = document.createElement('div');
+    div.dataset.qa = 'notification';
+    div.classList.add('success');
+    div.textContent = message;
+    document.body.appendChild(div);
+  })
+  .catch((message) => {
+    const div = document.createElement('div');
+    div.dataset.qa = 'notification';
+    div.classList.add('error');
+    div.textContent = message;
+    document.body.appendChild(div);
+  });
+
+
+const secondPromise = new Promise((resolve, reject) => {
+document.addEventListener ('click', (event) => {
+if (event.button === 0 || event.button === 2) {
+  resolve ('Second promise was resolved');
+}
+}, {once: true});
+});
+
+secondPromise
+  .then((message) => {
+    const div = document.createElement('div');
+    div.dataset.qa = 'notification';
+    div.classList.add('success');
+    div.textContent = message;
+    document.body.appendChild(div);
+  })
+  .catch();
+
+let leftClicked = false;
+let rightClicked = false;
+
+const thirdPromise = new Promise((resolve) => {
+  document.addEventListener('click', (event) => {
+    if (event.button === 0) leftClicked = true;
+    if (event.button === 2) rightClicked = true;
+
+    if (leftClicked && rightClicked) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+
+thirdPromise
+  .then((message) => {
+    const div = document.createElement('div');
+    div.dataset.qa = 'notification';
+    div.classList.add('success');
+    div.textContent = message;
+    document.body.appendChild(div);
+  })
+  .catch();
