6b7a6c2fb2966d3331092bda18ebac35052cee47
Artem Arseniev <144947394+Aarseniev@users.noreply.github.com>
2025-09-13 00:58:52 +0300

    fix issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,78 +18,88 @@ async function handleFirstPromise() {
   try {
     const firstPromise = new Promise((resolve, reject) => {
       let isSettled = false;
+      const timeoutId = setTimeout(() => {
+        if (!isSettled) {
+          // eslint-disable-next-line prefer-promise-reject-errors
+          reject('First promise was rejected');
+        }
+      }, 3000);
 
       document.addEventListener(
-        'click',
+        'mousedown',
         (e) => {
           if (e.button === 0 && isSettled === false) {
             resolve('First promise was resolved');
             isSettled = true;
+            clearTimeout(timeoutId);
           }
         },
         { once: true },
       );
-
-      setTimeout(() => {
-        if (!isSettled) {
-          reject(new Error('First promise was rejected'));
-        }
-      }, 3000);
     });
 
     const result = await firstPromise;
 
     showMessage(result);
   } catch (error) {
-    showMessage(error.message, true);
+    showMessage(error, true);
   }
 }
 
 handleFirstPromise();
 
 async function handleSecondPromise() {
-  const secondPromise = new Promise((resolve) => {
-    document.addEventListener(
-      'mousedown',
-      (e) => {
-        if (e.button === 0 || e.button === 2) {
-          resolve('Second promise was resolved');
-        }
-      },
-      { once: true },
-    );
-  });
+  try {
+    const secondPromise = new Promise((resolve) => {
+      document.addEventListener(
+        'mousedown',
+        (e) => {
+          if (e.button === 0 || e.button === 2) {
+            resolve('Second promise was resolved');
+          }
+        },
+        { once: true },
+      );
+    });
 
-  const result = await secondPromise;
+    const result = await secondPromise;
 
-  showMessage(result);
+    showMessage(result);
+  } catch (err) {
+    showMessage(err, true);
+  }
 }
 
 handleSecondPromise();
 
 async function handleThirdPromise() {
-  const thirdPromise = new Promise((resolve) => {
-    let leftClicked = false;
-    let rightClicked = false;
+  try {
+    const thirdPromise = new Promise((resolve) => {
+      let leftClicked = false;
+      let rightClicked = false;
 
-    document.addEventListener('mousedown', (e) => {
-      if (e.button === 0) {
-        leftClicked = true;
-      }
+      document.addEventListener('mousedown', function handler(e) {
+        if (e.button === 0) {
+          leftClicked = true;
+        }
 
-      if (e.button === 2) {
-        rightClicked = true;
-      }
+        if (e.button === 2) {
+          rightClicked = true;
+        }
 
-      if (leftClicked && rightClicked) {
-        resolve('Third promise was resolved');
-      }
+        if (leftClicked && rightClicked) {
+          document.removeEventListener('mousedown', handler);
+          resolve('Third promise was resolved');
+        }
+      });
     });
-  });
 
-  const result = await thirdPromise;
+    const result = await thirdPromise;
 
-  showMessage(result);
+    showMessage(result);
+  } catch (err) {
+    showMessage(err, true);
+  }
 }
 
 handleThirdPromise();
