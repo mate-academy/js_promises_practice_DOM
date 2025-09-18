31ce648b0724e89d0743e5d3a0f35ad20838bbe6
Vladyslav Stepanenko <vlad.step.186@gmail.com>
2025-09-18 12:54:49 +0300

    made three promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,82 @@
 'use strict';
+
+function showNotification(message, classes) {
+  const div = document.createElement('div');
+
+  div.className = classes;
+  div.textContent = message;
+  document.body.append(div);
+}
+
+const firstPromise = new Promise((resolve, reject) => {
+  const handleClick = () => {
+    resolve();
+    clearTimeout(timeoutId);
+    document.removeEventListener('click', handleClick);
+  };
+
+  document.addEventListener('click', handleClick);
+
+  const timeoutId = setTimeout(() => {
+    reject(new Error('Timeout error'));
+  }, 3000);
+});
+
+firstPromise
+  .then(() => {
+    showNotification('First promise was resolved', 'message');
+  })
+  .catch(() => {
+    showNotification('First promise was rejected!', 'message');
+  });
+
+const secondPromise = new Promise((resolve, reject) => {
+  const handleClick = () => {
+    resolve('Second promise was resolved');
+    document.removeEventListener('click', handleClick);
+    document.removeEventListener('contextmenu', handleContextMenu);
+  };
+
+  const handleContextMenu = () => {
+    resolve('Second promise was resolved');
+    document.removeEventListener('click', handleClick);
+    document.removeEventListener('contextmenu', handleContextMenu);
+  };
+
+  document.addEventListener('click', handleClick);
+  document.addEventListener('contextmenu', handleContextMenu);
+});
+
+secondPromise.then((message) => {
+  showNotification(message, 'message');
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  let leftClicked = false;
+  let rightClicked = false;
+
+  const handleClick = () => {
+    leftClicked = true;
+    checkClicks();
+  };
+
+  const handleContextMenu = () => {
+    rightClicked = true;
+    checkClicks();
+  };
+
+  const checkClicks = () => {
+    if (leftClicked && rightClicked) {
+      resolve('Third promise was resolved');
+      document.removeEventListener('click', handleClick);
+      document.removeEventListener('contextmenu', handleContextMenu);
+    }
+
+    document.addEventListener('click', handleClick);
+    document.addEventListener('contextmenu', handleContextMenu);
+  };
+
+  thirdPromise.then((message) => {
+    showNotification(message, 'message');
+  });
+});
