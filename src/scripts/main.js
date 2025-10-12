7282653d6ec3f1f3ef1dd81200547a8c7d3b1450
Bohdan Pidhrebelnyi <bodiiiiic@gmail.com>
2025-10-12 18:45:31 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,68 @@
 'use strict';
+
+let clicked = false;
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', (e) => {
+    if (e.button === 0) {
+      clicked = true;
+      resolve('First promise was resolved');
+    }
+  });
+
+  setTimeout(() => {
+    if (!clicked) {
+      reject(new Error('First promise was rejected'));
+    }
+  }, 3000);
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0 || e.button === 2) {
+      resolve('Second promise was resolved');
+    }
+  });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  let clickLeftMouse = false;
+  let clickRightMouse = false;
+
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      clickLeftMouse = true;
+    }
+
+    if (e.button === 2) {
+      clickRightMouse = true;
+    }
+
+    if (clickLeftMouse && clickRightMouse) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+function showNotification(type, message) {
+  const div = document.createElement('div');
+
+  div.dataset.qa = 'notification';
+  div.classList.add(type);
+  div.textContent = message;
+  document.body.appendChild(div);
+}
+
+firstPromise
+  .then((msg) => showNotification('success', msg))
+  .catch((err) => showNotification('error', err.message));
+
+secondPromise
+  .then((msg) => showNotification('success', msg))
+  .catch((err) => showNotification('error', err));
+
+thirdPromise
+  .then((msg) => showNotification('success', msg))
+  .catch((err) => showNotification('error', err));
+
+document.addEventListener('contextmenu', (e) => e.preventDefault());
