50d2e337e5f2d0430540e2f01a726d16662f862c
chumachenkoUA <kirito22122005@gmail.com>
2025-11-09 16:39:44 +0200

    first try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,112 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+  const onDown = (e) => {
+    if (e.button === 0) {
+      clearTimeout(timer);
+      resolve('First promise was resolved');
+    }
+  };
+
+  document.addEventListener('mousedown', onDown, { once: true });
+
+  const timer = setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener(
+    'mousedown',
+    (e) => {
+      if (e.button === 0 || e.button === 2) {
+        resolve('Second promise was resolved');
+      }
+    },
+    { once: true },
+  );
+});
+
+const leftClickPromise = new Promise((resolve) => {
+  document.addEventListener(
+    'mousedown',
+    (e) => {
+      if (e.button === 0) {
+        resolve('Third promise left click resolved');
+      }
+    },
+    { once: true },
+  );
+});
+const rightClickPromise = new Promise((resolve) => {
+  document.addEventListener(
+    'mousedown',
+    (e) => {
+      if (e.button === 2) {
+        resolve('Third promise right click resolved');
+      }
+    },
+    { once: true },
+  );
+});
+
+const thirdPromise = Promise.all([leftClickPromise, rightClickPromise]).then(
+  () => 'Third promise was resolved',
+);
+
+firstPromise.then(
+  (message) => {
+    const div = document.createElement('div');
+
+    div.className = 'notification success';
+    div.dataset.qa = 'notification';
+    div.textContent = message;
+    document.body.append(div);
+  },
+  (error) => {
+    const div = document.createElement('div');
+
+    div.className = 'notification error';
+    div.dataset.qa = 'notification';
+    div.textContent = error.message;
+    document.body.append(div);
+  },
+);
+
+secondPromise.then(
+  (message) => {
+    const div = document.createElement('div');
+
+    div.className = 'notification success';
+    div.dataset.qa = 'notification';
+    div.textContent = message;
+    document.body.append(div);
+  },
+  (error) => {
+    const div = document.createElement('div');
+
+    div.className = 'notification error';
+    div.dataset.qa = 'notification';
+    div.textContent = error.message;
+    document.body.append(div);
+  },
+);
+
+thirdPromise.then(
+  (message) => {
+    const div = document.createElement('div');
+
+    div.className = 'notification success';
+    div.dataset.qa = 'notification';
+    div.textContent = message;
+    document.body.append(div);
+  },
+  (error) => {
+    const div = document.createElement('div');
+
+    div.className = 'notification error';
+    div.dataset.qa = 'notification';
+    div.textContent = error.message;
+    document.body.append(div);
+  },
+);
