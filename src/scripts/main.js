7fc9de2083b06e942f7ae8f4eb9d38f1045fcf68
Anastasia <ahinata1717@gmail.com>
2025-12-09 18:51:32 +0200

    feat: implement promices

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,92 @@
 'use strict';
+
+const handleSuccess = (message) => {
+  const newDiv = document.createElement('div');
+
+  newDiv.textContent = message;
+  newDiv.setAttribute('data-qa', 'notification');
+  newDiv.className = 'success';
+  document.body.appendChild(newDiv);
+};
+
+const handleError = (errorMessage) => {
+  const errorDiv = document.createElement('div');
+
+  errorDiv.setAttribute('data-qa', 'notification');
+  errorDiv.className = 'error';
+  errorDiv.textContent = errorMessage;
+  document.body.appendChild(errorDiv);
+};
+
+const firstPromise = new Promise((resolve, reject) => {
+  const timeoutId = setTimeout(() => {
+    reject(new Error('Left click was not detected.'));
+  }, 3000);
+
+  document.addEventListener('click', () => {
+    clearTimeout(timeoutId);
+    resolve();
+  });
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  let isLeftClicked = false;
+  let isRightClicked = false;
+
+  function check() {
+    if (isLeftClicked || isRightClicked) {
+      return resolve();
+    }
+  }
+
+  document.addEventListener('click', () => {
+    isLeftClicked = true;
+    check();
+  });
+
+  document.addEventListener('contextmenu', () => {
+    isRightClicked = true;
+    check();
+  });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  let isLeftClicked = false;
+  let isRightClicked = false;
+
+  function check() {
+    if (isLeftClicked && isRightClicked) {
+      return resolve();
+    }
+  }
+
+  document.addEventListener('click', () => {
+    isLeftClicked = true;
+    check();
+  });
+
+  document.addEventListener('contextmenu', () => {
+    isRightClicked = true;
+    check();
+  });
+});
+
+firstPromise
+  .then(() => {
+    handleSuccess('First promise was resolved');
+  })
+  .catch(() => {
+    handleError('First promise was rejected');
+  });
+
+secondPromise.then(() => {
+  handleError('Second promise was resolved');
+});
+
+thirdPromise
+  .then(() => {
+    handleError('Third promise was resolved');
+  })
+  .catch(() => {
+    handleError('Third promise was rejected');
+  });
