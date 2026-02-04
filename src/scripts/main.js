5881081b2bf804c57f19af813cb146c3037e9064
unknown <dkuz6323@gmail.com>
2026-02-04 16:55:29 +0200

    completed tasks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,62 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+});
+
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener('mousedown', (events) => {
+    if (events.button === 0 || events.button === 2) {
+      resolve('Second promise was resolved');
+    }
+  });
+});
+
+const thirdPromise = new Promise((resolve) => {
+  let isLeftClicked = false;
+  let isRightClicked = false;
+
+  document.addEventListener('mousedown', (events) => {
+    if (events.button === 0) {
+      isLeftClicked = true;
+    } else if (events.button === 2) {
+      isRightClicked = true;
+    }
+
+    if (isLeftClicked && isRightClicked) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+function handleSuccess(message) {
+  const div = document.createElement('div');
+
+  div.setAttribute('data-qa', 'notification');
+  div.textContent = message;
+
+  div.className = 'success';
+
+  document.body.append(div);
+}
+
+function handleError(error) {
+  const div = document.createElement('div');
+
+  div.setAttribute('data-qa', 'notification');
+  div.textContent = error;
+
+  div.className = 'error';
+
+  document.body.append(div);
+}
+
+firstPromise.then(handleSuccess).catch(handleError);
+secondPromise.then(handleSuccess).catch(handleError);
+thirdPromise.then(handleSuccess).catch(handleError);
