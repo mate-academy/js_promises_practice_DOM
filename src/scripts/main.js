a7ea20cb8c19809e5f273e345d82e502d1070f95
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2026-02-20 13:55:32 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,91 @@
 'use strict';
+
+const body = document.querySelector('body');
+
+function showMessage(message, type) {
+  const div = document.createElement('div');
+
+  div.setAttribute('data-qa', 'notification');
+  div.classList.add(`${type}`);
+  div.textContent = message;
+
+  body.appendChild(div);
+}
+
+const firstPromise = new Promise((resolve, reject) => {
+  const id = setTimeout(
+    () => reject(new Error('First promise was rejected')),
+    3000,
+  );
+
+  document.addEventListener(
+    'mousedown',
+    (e) => {
+      if (e.button === 0) {
+        clearTimeout(id);
+        resolve('First promise was resolved');
+      }
+    },
+    { once: true },
+  );
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  let leftCheck = false;
+  let rightCheck = false;
+
+  document.addEventListener(
+    'mousedown',
+    (e) => {
+      if (e.button === 0) {
+        leftCheck = true;
+      }
+
+      if (e.button === 2) {
+        rightCheck = true;
+      }
+
+      if (leftCheck || rightCheck) {
+        resolve('Second promise was resolved');
+      }
+    },
+    { once: true },
+  );
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  let leftCheck = false;
+  let rightCheck = false;
+
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0) {
+      leftCheck = true;
+    }
+
+    if (e.button === 2) {
+      rightCheck = true;
+    }
+
+    if (leftCheck && rightCheck) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+firstPromise
+  .then((message) => {
+    showMessage(message, 'success');
+  })
+  .catch((message) => {
+    const errorMessage = message.message;
+
+    showMessage(errorMessage, 'error');
+  });
+
+secondPromise.then((message) => {
+  showMessage(message, 'success');
+});
+
+thirdPromise.then((message) => {
+  showMessage(message, 'success');
+});
