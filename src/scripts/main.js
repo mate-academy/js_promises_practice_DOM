ceb1fb00cdb1a593731f359ab405fb9fc77ee794
Banderos14 <anton.shyshenko@gmail.com>
2026-04-10 15:48:49 +0200

    feat: task_solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,93 @@
 'use strict';
+
+const logo = document.querySelector('.logo');
+
+const firstPromise = new Promise((resolve, reject) => {
+  const timer = setTimeout(() => {
+    reject(new Error('Timeout error'));
+  }, 3000);
+
+  logo.addEventListener('click', () => {
+    clearTimeout(timer);
+    resolve();
+  });
+});
+
+firstPromise
+  .then(() => {
+    const div = document.createElement('div');
+
+    div.className = 'message success';
+    div.dataset.qa = 'notification';
+    div.textContent = 'First promise was resolved';
+    document.body.append(div);
+  })
+  .catch(() => {
+    const div = document.createElement('div');
+
+    div.className = 'message error';
+    div.dataset.qa = 'notification';
+    div.textContent = 'First promise was rejected';
+    document.body.append(div);
+  });
+
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener('click', () => {
+    resolve();
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    resolve();
+  });
+});
+
+secondPromise.then(() => {
+  const div = document.createElement('div');
+
+  div.className = 'message success';
+  div.dataset.qa = 'notification';
+  div.textContent = 'Second promise was resolved';
+  document.body.append(div);
+});
+
+const leftClickPromise = new Promise((resolve) => {
+  document.addEventListener(
+    'click',
+    () => {
+      resolve();
+    },
+    { once: true },
+  );
+});
+
+const rightClickPromise = new Promise((resolve) => {
+  document.addEventListener(
+    'contextmenu',
+    (e) => {
+      e.preventDefault();
+      resolve();
+    },
+    { once: true },
+  );
+});
+
+const thirdPromise = Promise.all([leftClickPromise, rightClickPromise]);
+
+thirdPromise
+  .then(() => {
+    const div = document.createElement('div');
+
+    div.className = 'message success';
+    div.dataset.qa = 'notification';
+    div.textContent = 'Third promise was resolved';
+    document.body.append(div);
+  })
+  .catch(() => {
+    const div = document.createElement('div');
+
+    div.className = 'message error';
+    div.dataset.qa = 'notification';
+    div.textContent = 'Third promise was rejected';
+    document.body.append(div);
+  });
