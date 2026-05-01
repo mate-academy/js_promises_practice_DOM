120fd1343175227322215fed371d4ff62c12bb05
Viktor-1995 <madebyvityok@ukr.net>
2026-05-01 12:08:38 +0200

    solution 1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,90 @@
 'use strict';
+
+const body = document.getElementsByTagName('body')[0];
+
+const firstPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(leftClickError);
+  }, 3000);
+
+  document.addEventListener('click', (e) => {
+    if (e.button === 0) {
+      resolve();
+    }
+  });
+});
+
+function leftClickSuccess() {
+  const div = document.createElement('div');
+
+  div.dataset.qa = 'notification';
+  div.innerText = 'First promise was resolved';
+  div.className = 'success';
+  body.appendChild(div);
+}
+
+function leftClickError() {
+  const div = document.createElement('div');
+
+  div.dataset.qa = 'notification';
+  div.innerText = 'First promise was rejected';
+  div.className = 'error';
+  body.appendChild(div);
+}
+
+function anyClickSuccess() {
+  const div = document.createElement('div');
+
+  div.dataset.qa = 'notification';
+  div.innerText = 'Second promise was resolved';
+  div.className = 'success';
+  body.appendChild(div);
+}
+
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener('click', (e) => {
+    resolve();
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    resolve();
+  });
+});
+
+function bothClickSucces() {
+  const div = document.createElement('div');
+
+  div.dataset.qa = 'notification';
+  div.innerText = 'Third promise was resolved';
+  div.className = 'success';
+  body.appendChild(div);
+}
+
+const thirdPromise = new Promise((resolve) => {
+  let leftClicked = false;
+  let rightClicked = false;
+
+  function checkResolve() {
+    if (leftClicked && rightClicked) {
+      resolve();
+    }
+  }
+
+  document.addEventListener('click', (e) => {
+    if (e.button === 0) {
+      leftClicked = true;
+    }
+    checkResolve();
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    if (e.button === 2) {
+      rightClicked = true;
+    }
+    checkResolve();
+  });
+});
+
+firstPromise.then(leftClickSuccess).catch(leftClickError);
+secondPromise.then(anyClickSuccess);
+thirdPromise.then(bothClickSucces);
