bf45f7e9003fbbac5d4800d48b36fa906888e20c
Oksana Shtelma <oshtelma@gmail.com>
2026-05-06 15:33:40 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,21 @@
 'use strict';
 
+let left = false;
+let right = false;
+
 // eslint-disable-next-line no-shadow
 document.addEventListener('mousedown', function (event) {
   const firstPromise = new Promise((resolve, reject) => {
-    if (event.buttons === 1) {
-      event.preventDefault();
-      resolve(`First promise was resolved`);
-    }
-
-    setTimeout(() => {
+    const timerId = setTimeout(() => {
       // eslint-disable-next-line prefer-promise-reject-errors
       reject(`First promise was rejected`);
     }, 3000);
+
+    if (event.buttons === 1) {
+      event.preventDefault();
+      resolve(`First promise was resolved`);
+      clearTimeout(timerId);
+    }
   });
 
   firstPromise.then((result) => {
@@ -35,59 +39,65 @@ document.addEventListener('mousedown', function (event) {
 // eslint-disable-next-line no-shadow
 document.addEventListener('mousedown', (event) => {
   const secondPromise = new Promise((resolve, reject) => {
-    resolve(`Second promise was resolved`);
+    if (event.buttons === 1 || event.buttons === 2) {
+      event.preventDefault();
+      resolve(`Second promise was resolved`);
+    }
   });
 
-  if (event.buttons === 1 || event.buttons === 2) {
-    event.preventDefault();
+  secondPromise.then((result) => {
+    const div = document.createElement('div');
 
-    secondPromise.then((result) => {
-      const div = document.createElement('div');
+    div.setAttribute('data-qa', 'notification');
+    div.classList.add(`success`);
+    document.body.appendChild(div);
+  });
 
-      div.setAttribute('data-qa', 'notification');
-      div.classList.add(`success`);
-      document.body.appendChild(div);
-    });
+  // eslint-disable-next-line handle-callback-err
+  secondPromise.catch((error) => {
+    const div = document.createElement('div');
 
-    // eslint-disable-next-line handle-callback-err
-    secondPromise.catch((error) => {
-      const div = document.createElement('div');
-
-      div.setAttribute('data-qa', 'notification');
-      div.classList.add(`error`);
-      document.body.appendChild(div);
-    });
-  }
+    div.setAttribute('data-qa', 'notification');
+    div.classList.add(`error`);
+    document.body.appendChild(div);
+  });
 });
 
 // eslint-disable-next-line no-shadow
 document.addEventListener('mousedown', (event) => {
-  let left = false;
-  let right = false;
-
-  if (event.buttons === 1) {
-    // eslint-disable-next-line no-unused-vars
-    left = true;
-  }
-
-  if (event.buttons === 2) {
-    // eslint-disable-next-line no-unused-vars
-    right = true;
-  }
-
   const thirdPromise = new Promise((resolve, reject) => {
-    resolve(`Third promise was resolved`);
+    if (event.buttons === 1) {
+      // eslint-disable-next-line no-unused-vars
+      left = true;
+    }
+
+    if (event.buttons === 2) {
+      // eslint-disable-next-line no-unused-vars
+      right = true;
+    }
+
+    if (left === true && right === true) {
+      event.preventDefault();
+      resolve(`Third promise was resolved`);
+      left = false;
+      right = false;
+    }
   });
 
-  if (left === true && right === true) {
-    event.preventDefault();
+  thirdPromise.then((result) => {
+    const div = document.createElement('div');
 
-    thirdPromise.then((result) => {
-      const div = document.createElement('div');
+    div.setAttribute('data-qa', 'notification');
+    div.classList.add(`success`);
+    document.body.appendChild(div);
+  });
 
-      div.attributes = 'data-qa="notification';
-      div.classList.add(`success`);
-      document.body.appendChild(div);
-    });
-  }
+  // eslint-disable-next-line handle-callback-err
+  thirdPromise.catch((error) => {
+    const div = document.createElement('div');
+
+    div.setAttribute('data-qa', 'notification');
+    div.classList.add(`error`);
+    document.body.appendChild(div);
+  });
 });
