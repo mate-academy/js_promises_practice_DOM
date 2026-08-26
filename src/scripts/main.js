3d861d8b2340655d828f80026e1f3a677511a8eb
jkumorek01 <jkumorek01@gmail.com>
2026-08-26 13:35:20 +0200

    Second task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const p1 = new Promise((resolve, reject) => {
+const firstPromise = new Promise((resolve, reject) => {
   document.addEventListener('mousedown', (clickEvent) => {
     if (clickEvent.button === 0) {
       resolve('First promise was resolved');
@@ -12,7 +12,7 @@ const p1 = new Promise((resolve, reject) => {
   }, 3000);
 });
 
-const p2 = new Promise((resolve) => {
+const secondPromise = new Promise((resolve) => {
   document.addEventListener('mousedown', (clickEvent2) => {
     if (clickEvent2.button === 0 || clickEvent2.button === 2) {
       resolve(`Second promise was resolved`);
@@ -20,7 +20,7 @@ const p2 = new Promise((resolve) => {
   });
 });
 
-const p3 = new Promise((resolve, reject) => {
+const thirdPromise = new Promise((resolve, reject) => {
   let leftClick = false;
   let rightClick = false;
 
@@ -39,7 +39,7 @@ const p3 = new Promise((resolve, reject) => {
   });
 });
 
-p1.then((info) => {
+firstPromise.then((info) => {
   const div = document.createElement('div');
 
   div.dataset.qa = 'notification';
@@ -57,7 +57,7 @@ p1.then((info) => {
   document.body.append(div);
 });
 
-p2.then((info) => {
+secondPromise.then((info) => {
   const div = document.createElement('div');
 
   div.dataset.qa = 'notification';
@@ -75,7 +75,7 @@ p2.then((info) => {
   document.body.append(div);
 });
 
-p3.then((info) => {
+thirdPromise.then((info) => {
   const div = document.createElement('div');
 
   div.dataset.qa = 'notification';
